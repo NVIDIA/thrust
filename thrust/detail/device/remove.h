aa9decf95d93f391e1ca3be6810242bf6f504285
Jared Hoberock <jaredhoberock@gmail.com>
2009-08-11 03:34:06 +0000

    Use raw_buffer in remove.
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40332

--- thrust/detail/device/remove.h
+++ thrust/detail/device/remove.h
@@ -29,8 +29,7 @@
 #include <thrust/scan.h>
 #include <thrust/scatter.h>
 #include <thrust/device_ptr.h>
-#include <thrust/device_malloc.h>
-#include <thrust/device_free.h>
+#include <thrust/detail/raw_buffer.h>
 
 namespace thrust
 {
@@ -53,18 +52,16 @@ template<typename ForwardIterator,
   typedef typename thrust::iterator_traits<ForwardIterator>::value_type InputType;
 
   // create temporary storage for an intermediate result
-  thrust::device_ptr<InputType> temp = thrust::device_malloc<InputType>(end - begin);
+  typedef raw_buffer<InputType,experimental::space::device> RawBuffer;
+  RawBuffer temp(end - begin);
 
   // remove into temp
-  thrust::device_ptr<InputType> new_end = thrust::remove_copy_if(begin, end, temp, pred);
+  typename RawBuffer::iterator new_end = thrust::remove_copy_if(begin, end, temp.begin(), pred);
 
   // copy temp to the original range
-  thrust::copy(temp, new_end, begin);
-  
-  // free temp space
-  thrust::device_free(temp);
+  thrust::copy(temp.begin(), new_end, begin);
 
-  return begin + (new_end - temp);
+  return begin + (new_end - temp.begin());
 } 
 
 
@@ -87,32 +84,29 @@ template<typename InputIterator,
     thrust::unary_negate<Predicate> not_pred(pred);
 
     // evaluate not_pred on [begin,end), store result to temp vector
-    thrust::device_ptr<difference_type> result_of_not_pred = thrust::device_malloc<difference_type>(n);
+    raw_buffer<difference_type, experimental::space::device> result_of_not_pred(n);
 
     thrust::transform(begin,
-                       end,
-                       result_of_not_pred,
-                       not_pred);
+                      end,
+                      result_of_not_pred.begin(),
+                      not_pred);
 
     // scan the pred result to a temp vector
-    thrust::device_ptr<difference_type> not_pred_scatter_indices = thrust::device_malloc<difference_type>(n);
-    thrust::exclusive_scan(result_of_not_pred,
-                            result_of_not_pred + n,
-                            not_pred_scatter_indices);
+    raw_buffer<difference_type, experimental::space::device> not_pred_scatter_indices(n);
+    thrust::exclusive_scan(result_of_not_pred.begin(),
+                           result_of_not_pred.end(),
+                           not_pred_scatter_indices.begin());
 
     // scatter the true partition
     thrust::scatter_if(begin,
-                        end,
-                        not_pred_scatter_indices,
-                        result_of_not_pred,
-                        result);
+                       end,
+                       not_pred_scatter_indices.begin(),
+                       result_of_not_pred.begin(),
+                       result);
 
     // find the end of the new sequence
     size_of_new_sequence = not_pred_scatter_indices[n - 1]
                          + (not_pred(*(end-1)) ? 1 : 0);
-
-    thrust::device_free(result_of_not_pred);
-    thrust::device_free(not_pred_scatter_indices);
   } // end if
 
   return result + size_of_new_sequence;
