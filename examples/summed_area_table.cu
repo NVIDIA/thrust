1c500c0a7d65993580c3e3b683ab247e1b852073
Nathan Bell <wnbell@gmail.com>
2009-08-30 23:42:14 +0000

    added summed area table example
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40406

---
+++ examples/summed_area_table.cu
@@ -0,0 +1,118 @@
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/gather.h>
+#include <thrust/segmented_scan.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/iterator/transform_iterator.h>
+#include <iostream>
+#include <iomanip>
+
+// This example computes a summed area table using segmented scan
+// http://en.wikipedia.org/wiki/Summed_area_table
+
+
+
+// convert a linear index to a linear index in the transpose 
+struct transpose_index
+{
+    size_t m, n;
+
+    typedef size_t result_type;
+    
+    __host__ __device__
+    transpose_index(size_t _m, size_t _n) : m(_m), n(_n) {}
+
+    __host__ __device__
+    size_t operator()(size_t linear_index)
+    {
+        size_t i = linear_index / n;
+        size_t j = linear_index % n;
+
+        return m * j + i;
+    }
+};
+
+// convert a linear index to a row index
+struct row_index
+{
+    size_t n;
+    
+    typedef size_t result_type;
+
+    __host__ __device__
+    row_index(size_t _n) : n(_n) {}
+
+    __host__ __device__
+    size_t operator()(size_t i)
+    {
+        return i / n;
+    }
+};
+
+// transpose an M-by-N array
+template <typename T>
+void transpose(size_t m, size_t n, thrust::device_vector<T>& src, thrust::device_vector<T>& dst)
+{
+    thrust::experimental::counting_iterator<size_t> indices(0);
+    
+    thrust::gather(dst.begin(), dst.end(),
+                   make_transform_iterator(indices, transpose_index(n, m)),
+                   src.begin());
+}
+
+
+// scan the rows of an M-by-N array
+template <typename T>
+void scan_horizontally(size_t m, size_t n, thrust::device_vector<T>& d_data)
+{
+    thrust::experimental::counting_iterator<size_t> indices(0);
+
+    thrust::experimental::inclusive_segmented_scan(d_data.begin(), d_data.end(),
+                                     make_transform_iterator(indices, row_index(n)),
+                                     d_data.begin());
+}
+
+// print an M-by-N array
+template <typename T>
+void print(size_t m, size_t n, thrust::device_vector<T>& d_data)
+{
+    thrust::host_vector<T> h_data = d_data;
+
+    for(size_t i = 0; i < m; i++)
+    {
+        for(size_t j = 0; j < n; j++)
+            std::cout << std::setw(8) << h_data[i * n + j] << " ";
+        std::cout << "\n";
+    }
+}
+
+int main(void)
+{
+    size_t m = 3; // number of rows
+    size_t n = 4; // number of columns
+
+    // 2d array stored in row-major order [(0,0), (0,1), (0,2) ... ]
+    thrust::device_vector<int> data(m * n, 1);
+
+    std::cout << "[step 0] initial array" << std::endl;
+    print(m, n, data);
+
+    std::cout << "[step 1] scan horizontally" << std::endl;
+    scan_horizontally(m, n, data);
+    print(m, n, data);
+
+    std::cout << "[step 2] transpose array" << std::endl;
+    thrust::device_vector<int> temp(m * n);
+    transpose(m, n, data, temp);
+    print(n, m, temp);
+
+    std::cout << "[step 3] scan transpose horizontally" << std::endl;
+    scan_horizontally(n, m, temp);
+    print(n, m, temp);
+
+    std::cout << "[step 4] transpose the transpose" << std::endl;
+    transpose(n, m, temp, data);
+    print(m, n, data);
+
+    return 0;
+}
