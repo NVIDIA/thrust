040df7cd55cc7b1fbffc85388c498e03fc9b6e94
Nathan Bell <wnbell@gmail.com>
2009-09-03 01:41:52 +0000

    updated complicated complicated_transform example to use zip_iterator (AKA the new hotness)
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40419

--- examples/complicated_transformation.cu
+++ examples/complicated_transformation.cu
@@ -1,57 +1,57 @@
 #include <thrust/for_each.h>
-#include <thrust/iterator/counting_iterator.h>
-#include <thrust/generate.h>
-#include <thrust/iterator/iterator_traits.h>
-#include <thrust/device_ptr.h>
-#include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
+#include <thrust/iterator/zip_iterator.h>
 #include <iostream>
-#include <cstdlib>
 
-// this example shows how to implement a complicated
-// transformation that requires 3 inputs and 1 output
-// using for_each and counting_iterator
+// This example shows how to implement a complicated transformation that
+// requires 3 inputs and 1 output using for_each and zip_iterator.
+// Iterators for all four vectors are combined with the zip_iterator 
+// into a single sequence of tuples (i.e. they are "zipped" together).
+// The complicated_functor receives a tuple that contains four elements,
+// which are refrences to values in each of the four sequences.
+// When we access the tuple 't' with the get() function
+//      get<0>(t) returns a reference to A[i]
+//      get<1>(t) returns a reference to B[i]
+//      get<2>(t) returns a reference to C[i]
+//      get<3>(t) returns a reference to D[i]
+//
+// In this example, we can implement the transformation,
+//      D[i] = A[i] + B[i] * C[i];
+// by invoking complicated_functor() on each of the tuples using for_each.
 
 struct complicated_functor
 {
-    const float * A;
-    const float * B;    
-    const float * C;    
-          float * D;
-
-    complicated_functor(const float * _A, const float * _B, const float * _C, float * _D):
-        A(_A), B(_B), C(_C), D(_D) {}
-
-    template <typename IndexType>
+    template <typename Tuple>
     __host__ __device__
-    void operator()(const IndexType & i){
-        D[i] = A[i] + B[i] * C[i];
+    void operator()(Tuple t)
+    {
+        // D[i] = A[i] + B[i] * C[i];
+        thrust::get<3>(t) = thrust::get<0>(t) + thrust::get<1>(t) * thrust::get<2>(t);
     }
 };
 
 
 int main(void)
 {
+    // allocate storage
     thrust::device_vector<float> A(5);
     thrust::device_vector<float> B(5);
     thrust::device_vector<float> C(5);
     thrust::device_vector<float> D(5);
 
+    // initialize input vectors
     A[0] = 3;  B[0] = 6;  C[0] = 2; 
     A[1] = 4;  B[1] = 7;  C[1] = 5; 
     A[2] = 0;  B[2] = 2;  C[2] = 7; 
     A[3] = 8;  B[3] = 1;  C[3] = 4; 
     A[4] = 2;  B[4] = 8;  C[4] = 3; 
 
-    thrust::experimental::counting_iterator<int, thrust::device_space_tag> begin(0);
+    // apply the transformation
+    thrust::for_each(thrust::experimental::make_zip_iterator(thrust::make_tuple(A.begin(), B.begin(), C.begin(), D.begin())),
+                     thrust::experimental::make_zip_iterator(thrust::make_tuple(A.end(),   B.end(),   C.end(),   D.end())),
+                     complicated_functor());
 
-    complicated_functor op(thrust::raw_pointer_cast(&A[0]), 
-                           thrust::raw_pointer_cast(&B[0]), 
-                           thrust::raw_pointer_cast(&C[0]),
-                           thrust::raw_pointer_cast(&D[0]));
-
-    thrust::for_each(begin, begin + 5, op);
-
+    // print the output
     for(int i = 0; i < 5; i++)
         std::cout << A[i] << " + " << B[i] << " * " << C[i] << " = " << D[i] << std::endl;
 }
