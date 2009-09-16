26348fa8bad336092f270e9bcaf732e1b5746fa9
Nathan Bell <wnbell@gmail.com>
2009-09-16 23:25:40 +0000

    added documentation to norm example added stream compaction example
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40493

--- examples/norm.cu
+++ examples/norm.cu
@@ -4,6 +4,18 @@
 #include <thrust/host_vector.h>
 #include <cmath>
 
+//   This example computes the norm [1] of a vector.  The norm is 
+// computed by squaring all numbers in the vector, summing the 
+// squares, and taking the square root of the sum of squares.  In
+// Thrust this operation is efficiently implemented with the 
+// transform_reduce() algorith.  Specifically, we first transform
+// x -> x^2 and the compute a standard plus reduction.  Since there
+// is no built-in functor for squaring numbers, we define our own
+// square functor.
+//
+// [1] http://en.wikipedia.org/wiki/Norm_(mathematics)#Euclidean_norm
+
+
 // square<T> computes the square of a number f(x) -> x*x
 template <typename T>
 struct square
@@ -30,7 +42,7 @@ int main(void)
     // compute norm
     float norm = std::sqrt( thrust::transform_reduce(d_x.begin(), d_x.end(), unary_op, init, binary_op) );
 
-    std::cout << norm << std::endl;
+    std::cout << "norm is " << norm << std::endl;
 
     return 0;
 }
