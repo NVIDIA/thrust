e19edb82345c298c9abced18932fff1325179729
Nathan Bell <wnbell@gmail.com>
2009-10-23 14:23:23 +0000

    added example sorting AoS vs. SoA
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40549

---
+++ examples/sorting_aos_vs_soa.cu
@@ -0,0 +1,97 @@
+#include <thrust/device_vector.h>
+#include <thrust/sort.h>
+
+#include <cuda.h>
+#include <cstdlib>
+
+// This examples compares sorting performance using Array of Structures (AoS)
+// and Structure of Arrays (SoA) data layout.  Legacy applications will often
+// store data in C/C++ structs, such as MyStruct defined below.  Although 
+// Thrust can process array of structs, it is typically less efficient than
+// the equivalent structure of arrays layout.  In this particular example,
+// the optimized SoA approach is approximately *five times faster* than the
+// traditional AoS method.  Therefore, it is almost always worthwhile to
+// convert AoS data structures to SoA.
+
+struct MyStruct
+{
+    int key;
+    float value;
+
+    bool operator<(const MyStruct other) const
+    {
+        return key < other.key;
+    }
+};
+
+void initialize_keys(thrust::device_vector<int>& keys)
+{
+    thrust::host_vector<int> h_keys(keys.size());
+
+    for(size_t i = 0; i < h_keys.size(); i++)
+        h_keys[i] = rand();
+
+    keys = h_keys;
+}
+
+
+void initialize_keys(thrust::device_vector<MyStruct>& structures)
+{
+    thrust::host_vector<MyStruct> h_structures(structures.size());
+
+    for(size_t i = 0; i < h_structures.size(); i++)
+        h_structures[i].key = rand();
+
+    structures = h_structures;
+}
+
+int main(void)
+{
+    size_t N = 1000000;
+    cudaEvent_t start;
+    cudaEvent_t end;
+    float elapsed_time;
+    
+    cudaEventCreate(&start);
+    cudaEventCreate(&end);
+
+    // Sort Key-Value pairs using Array of Structures (AoS) storage 
+    {
+        thrust::device_vector<MyStruct> structures(N);
+
+        initialize_keys(structures);
+
+        cudaEventRecord(start,0);
+
+        thrust::sort(structures.begin(), structures.end());
+
+        cudaEventSynchronize(end);
+        cudaEventRecord(end,0);
+        cudaEventSynchronize(end);
+        cudaEventElapsedTime(&elapsed_time, start, end);
+
+        std::cout << "AoS sort took " << elapsed_time << " milliseconds" << std::endl;
+    }
+   
+    // Sort Key-Value pairs using Structure of Arrays (SoA) storage 
+    {
+        thrust::device_vector<int>   keys(N);
+        thrust::device_vector<float> values(N);
+
+        initialize_keys(keys);
+
+        cudaEventRecord(start,0);
+
+        thrust::sort_by_key(keys.begin(), keys.end(), values.begin());
+
+        cudaThreadSynchronize();
+        cudaEventRecord(end,0);
+        cudaEventSynchronize(end);
+        cudaEventElapsedTime(&elapsed_time, start, end);
+
+        std::cout << "SoA sort took " << elapsed_time << " milliseconds" << std::endl;
+    }
+
+    return 0;
+}
+
