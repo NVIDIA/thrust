3f59badfdf78d629390246778aa4366c1f5caaa9
Jared Hoberock <jaredhoberock@gmail.com>
2009-12-27 04:26:03 +0000

    xor_combine_engine needed these changes to mpl/math.h.
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40701

--- thrust/detail/mpl/math.h
+++ thrust/detail/mpl/math.h
@@ -66,6 +66,104 @@ struct log2
 };
 
 
+template <typename T, T lhs, T rhs>
+struct min
+{
+  static const T value = (lhs < rhs) ? lhs : rhs;
+};
+
+
+template <typename T, T lhs, T rhs>
+struct max
+{
+  static const T value = (!(lhs < rhs)) ? lhs : rhs;
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct mul
+{
+  static const result_type value = x * y;
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct mod
+{
+  static const result_type value = x % y;
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct div
+{
+  static const result_type value = x / y;
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct geq
+{
+  static const bool value = x >= y;
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct lt
+{
+  static const bool value = x < y;
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct gt
+{
+  static const bool value = x > y;
+};
+
+
+template<bool x, bool y>
+  struct or_
+{
+  static const bool value = (x || y);
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct bit_and
+{
+  static const result_type value = x & y;
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct plus
+{
+  static const result_type value = x + y;
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct minus
+{
+  static const result_type value = x - y;
+};
+
+
+template<typename result_type, result_type x, result_type y>
+  struct equal
+{
+  static const bool value = x == y;
+};
+
+
+template<typename result_type, result_type x>
+  struct is_odd
+{
+  static const bool value = x & 1;
+};
+
+
 } // end namespace math
 
 } // end namespace mpl
