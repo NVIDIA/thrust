d3beb77e314bd035c67e7c684441473c38934e6a
Nathan Bell <wnbell@gmail.com>
2010-01-23 09:34:02 +0000

    expanded permutation_iterator unit tests
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40738

--- testing/permutation_iterator.cu
+++ testing/permutation_iterator.cu
@@ -1,11 +1,9 @@
 #include <thrusttest/unittest.h>
 #include <thrust/iterator/permutation_iterator.h>
 
-#include <thrust/copy.h>
 #include <thrust/reduce.h>
+#include <thrust/transform_reduce.h>
 #include <thrust/sequence.h>
-#include <thrust/iterator/counting_iterator.h>
-#include <thrust/iterator/transform_iterator.h>
 
 template <class Vector>
 void TestPermutationIterator(void)
@@ -25,15 +23,28 @@ void TestPermutationIterator(void)
     indices[3] = 7;
    
     // construct transform_iterator
-    thrust::permutation_iterator<Iterator, Iterator> iter(input.begin(), indices.begin());
+    thrust::permutation_iterator<Iterator, Iterator> p_source(source.begin(), indices.begin());
 
-    thrust::copy(iter, iter + 4, output.begin());
+    // copy from source to output
+    thrust::copy(p_source, p_source + 4, output.begin());
 
     ASSERT_EQUAL(output[0], 4);
     ASSERT_EQUAL(output[1], 1);
     ASSERT_EQUAL(output[2], 6);
     ASSERT_EQUAL(output[3], 8);
+    
+    // copy from output to source
+    thrust::fill(output.begin(), output.end(), 10);
+    thrust::copy(output.begin(), output.end(), p_source);
 
+    ASSERT_EQUAL(source[0], 10);
+    ASSERT_EQUAL(source[1],  2);
+    ASSERT_EQUAL(source[2],  3);
+    ASSERT_EQUAL(source[3], 10);
+    ASSERT_EQUAL(source[4],  5);
+    ASSERT_EQUAL(source[5], 10);
+    ASSERT_EQUAL(source[6],  7);
+    ASSERT_EQUAL(source[7], 10);
 }
 DECLARE_VECTOR_UNITTEST(TestPermutationIterator);
 
@@ -55,7 +66,7 @@ void TestMakePermutationIterator(void)
     indices[3] = 7;
    
     // construct transform_iterator
-    thrust::permutation_iterator<Iterator, Iterator> iter(input.begin(), indices.begin());
+    thrust::permutation_iterator<Iterator, Iterator> iter(source.begin(), indices.begin());
 
     thrust::copy(thrust::make_permutation_iterator(source.begin(), indices.begin()),
                  thrust::make_permutation_iterator(source.begin(), indices.begin()) + 4,
@@ -67,4 +78,39 @@ void TestMakePermutationIterator(void)
     ASSERT_EQUAL(output[3], 8);
 }
 DECLARE_VECTOR_UNITTEST(TestMakePermutationIterator);
+
+template <typename Vector>
+void TestPermutationIteratorReduce(void)
+{
+    typedef typename Vector::value_type T;
+    typedef typename Vector::iterator Iterator;
+
+    Vector source(8);
+    Vector indices(4);
+    Vector output(4, 10);
+    
+    // initialize input
+    thrust::sequence(source.begin(), source.end(), 1);
+
+    indices[0] = 3;
+    indices[1] = 0;
+    indices[2] = 5;
+    indices[3] = 7;
+   
+    // construct transform_iterator
+    thrust::permutation_iterator<Iterator, Iterator> iter(source.begin(), indices.begin());
+
+    T result1 = thrust::reduce(thrust::make_permutation_iterator(source.begin(), indices.begin()),
+                               thrust::make_permutation_iterator(source.begin(), indices.begin()) + 4);
+
+    ASSERT_EQUAL(result1, 19);
+    
+    T result2 = thrust::transform_reduce(thrust::make_permutation_iterator(source.begin(), indices.begin()),
+                                         thrust::make_permutation_iterator(source.begin(), indices.begin()) + 4,
+                                         thrust::negate<T>(),
+                                         T(0),
+                                         thrust::plus<T>());
+    ASSERT_EQUAL(result2, -19);
+};
+DECLARE_VECTOR_UNITTEST(TestPermutationIteratorReduce);
 
