59e668debfadd08b69367e19f3bdefe7934130cd
Jared Hoberock <jaredhoberock@gmail.com>
2010-01-28 06:56:38 +0000

    Migrate lexicographical_sort to thrust::next::gather.
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40760

--- examples/lexicographical_sort.cu
+++ examples/lexicographical_sort.cu
@@ -18,7 +18,7 @@ void update_permutation(KeyVector& keys, PermutationVector& permutation)
     KeyVector temp(keys.size());
 
     // permute the keys with the current reordering
-    thrust::gather(temp.begin(), temp.end(), permutation.begin(), keys.begin());
+    thrust::next::gather(permutation.begin(), permutation.end(), keys.begin(), temp.begin());
 
     // stable_sort the permuted keys and update the permutation
     thrust::stable_sort_by_key(temp.begin(), temp.end(), permutation.begin());
@@ -32,7 +32,7 @@ void apply_permutation(KeyVector& keys, PermutationVector& permutation)
     KeyVector temp(keys.begin(), keys.end());
 
     // permute the keys
-    thrust::gather(keys.begin(), keys.end(), permutation.begin(), temp.begin());
+    thrust::next::gather(permutation.begin(), permutation.end(), temp.begin(), keys.begin());
 }
 
 
