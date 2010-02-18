3eaffba03b59dc0ff632cac821b0e78196946f55
Jared Hoberock <jaredhoberock@gmail.com>
2010-02-18 06:09:21 +0000

    Add documentation for THRUST_VERSION, THRUST_MAJOR_VERSION, THRUST_MINOR_VERSION, and THRUST_SUBMINOR_VERSION.
    Add declarations of namespaces thrust, thrust::deprecated, and thrust::next in version.h and add documentation for them.
    
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40825

--- thrust/version.h
+++ thrust/version.h
@@ -14,6 +14,13 @@
  *  limitations under the License.
  */
 
+/*! \file version.h
+ *  \brief Compile-time macros encoding Thrust release version.
+ *
+ *         <thrust/version.h> is the only Thrust header that is guaranteed to
+ *         change with every thrust release.
+ */
+
 #pragma once
 
 #include <thrust/detail/config.h>
@@ -25,8 +32,62 @@
 //  THRUST_VERSION / 100 % 1000 is the minor version
 //  THRUST_VERSION / 100000 is the major version
 
+/*! \def THRUST_VERSION
+ *  \brief The preprocessor macro \p THRUST_VERSION encodes the version
+ *         number of the Thrust library.
+ *
+ *         <tt>THRUST_VERSION % 100</tt> is the sub-minor version.
+ *         <tt>THRUST_VERSION / 100 % 1000</tt> is the minor version.
+ *         <tt>THRUST_VERSION / 100000</tt> is the major version.
+ */
 #define THRUST_VERSION 100101
+
+/*! \def THRUST_MAJOR_VERSION
+ *  \brief The preprocessor macro \p THRUST_MAJOR_VERSION encodes the
+ *         major version number of the Thrust library.
+ */
 #define THRUST_MAJOR_VERSION     (THRUST_VERSION / 100000)
+
+/*! \def THRUST_MINOR_VERSION
+ *  \brief The preprocessor macro \p THRUST_MINOR_VERSION encodes the
+ *         minor version number of the Thrust library.
+ */
 #define THRUST_MINOR_VERSION     (THRUST_VERSION / 100 % 1000)
+
+/*! \def THRUST_SUBMINOR_VERSION
+ *  \brief The preprocessor macro \p THRUST_SUBMINOR_VERSION encodes the
+ *         sub-minor version number of the Thrust library.
+ */
 #define THRUST_SUBMINOR_VERSION  (THRUST_VERSION % 100)
+
+// Declare these namespaces here for the purpose of Doxygenating them
+
+/*! \namespace thrust
+ *  \brief \p thrust is the top-level namespace which contains all Thrust
+ *         functions and types.
+ */
+namespace thrust
+{
+
+/*! \namespace deprecated
+ *  \brief The \p deprecated namespace contains functionality which is scheduled
+ *         to be removed in a future version of Thrust.
+ *
+ *         Users of functionality in namespace \p deprecated are encouraged to
+ *         migrate their code to functionality offered elsewhere.
+ */
+namespace deprecated
+{
+}
+
+/*! \namespace next
+ *  \brief The \p next namespace contains functionality which is scheduled to be
+ *         promoted to the top-level \p thrust namespace in a future version of
+ *         Thrust.
+ */
+namespace next
+{
+}
+  
+}
 
