a861526ae1a4dba326290f526c45d524252c91c7
Nathan Bell <wnbell@gmail.com>
2010-03-30 14:30:31 +0000

    added Makefile for unit tests
    --HG--
    extra : convert_revision : svn%3A83215879-3e5a-4751-8c9d-778f44bb06a5/trunk%40957

--- testing/device_delete.cu
+++ testing/device_delete.cu
@@ -27,20 +27,20 @@ struct Foo
 void TestDeviceDeleteDestructorInvocation(void)
 {
   KNOWN_FAILURE;
-
-  thrust::device_vector<bool> destructor_flag(1, false);
-
-  thrust::device_ptr<Foo> foo_ptr  = thrust::device_new<Foo>();
-
-  Foo exemplar;
-  exemplar.set_me_upon_destruction = thrust::raw_pointer_cast(&destructor_flag[0]);
-  *foo_ptr = exemplar;
-
-  ASSERT_EQUAL(false, destructor_flag[0]);
-
-  thrust::device_delete(foo_ptr);
-
-  ASSERT_EQUAL(true, destructor_flag[0]);
+//
+//  thrust::device_vector<bool> destructor_flag(1, false);
+//
+//  thrust::device_ptr<Foo> foo_ptr  = thrust::device_new<Foo>();
+//
+//  Foo exemplar;
+//  exemplar.set_me_upon_destruction = thrust::raw_pointer_cast(&destructor_flag[0]);
+//  *foo_ptr = exemplar;
+//
+//  ASSERT_EQUAL(false, destructor_flag[0]);
+//
+//  thrust::device_delete(foo_ptr);
+//
+//  ASSERT_EQUAL(true, destructor_flag[0]);
 }
 DECLARE_UNITTEST(TestDeviceDeleteDestructorInvocation);
 
