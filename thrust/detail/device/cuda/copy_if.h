ef14172e97c7e66c52593a9053ca5a1865098c19
Nathan Bell <wnbell@gmail.com>
2010-07-09 18:13:20 -0800

    added draft implementation of cuda::copy_if

---
+++ thrust/detail/device/cuda/copy_if.h
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2008-2010 NVIDIA Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#pragma once
+
+namespace thrust
+{
+namespace detail
+{
+namespace device
+{
+namespace cuda
+{
+
+template<typename InputIterator1,
+         typename InputIterator2,
+         typename OutputIterator,
+         typename Predicate>
+   OutputIterator copy_if(InputIterator1 first,
+                          InputIterator1 last,
+                          InputIterator2 stencil,
+                          OutputIterator result,
+                          Predicate pred);
+
+} // end namespace cuda
+} // end namespace device
+} // end namespace detail
+} // end namespace thrust
+
+#include <thrust/detail/device/cuda/copy_if.inl>
+
