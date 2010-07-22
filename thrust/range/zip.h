848892bd5f361a3408c09d63df0963d41c16b4aa
Jared Hoberock <jaredhoberock@gmail.com>
2010-07-21 17:05:55 -0700

    backout

--- thrust/range/zip.h
+++ thrust/range/zip.h
@@ -22,7 +22,7 @@
 #include <thrust/range/end.h>
 #include <thrust/range/detail/zip_result.h>
 
-// spawn more overloads
+// XXX we will need to make a script to produce all the overloads
 
 namespace thrust
 {
