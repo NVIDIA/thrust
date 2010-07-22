2925398bed50d36e6fa16e148048abad8f1b51e2
Jared Hoberock <jaredhoberock@gmail.com>
2010-07-21 17:03:37 -0700

    backout

--- thrust/detail/has_nested_type.h
+++ thrust/detail/has_nested_type.h
@@ -16,17 +16,15 @@
 
 #pragma once
 
-#include <thrust/detail/type_traits.h>
-
 #define __THRUST_DEFINE_HAS_NESTED_TYPE(trait_name, nested_type_name) \
 template<typename T> \
   struct trait_name  \
 {                    \
   typedef char yes_type; \
   typedef int  no_type;  \
-  template<typename S> static yes_type test(typename S::nested_type_name *); \
+  template<typename S> static yes_type test(typename S::##nested_type_name *); \
   template<typename S> static no_type  test(...); \
-  static bool const value = sizeof(test<T>(0)) == sizeof(yes_type);\
+  static bool const value = sizeof(test<T>(0) == sizeof(yes_type);\
   typedef thrust::detail::integral_constant<bool, value> type;\
-};
+}; \
 
