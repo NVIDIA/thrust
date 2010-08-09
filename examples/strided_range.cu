f8c39560b3b54315fac4a3064ab6f0799ef7212a
Nathan Bell <wnbell@gmail.com>
2010-08-09 14:38:07 -0800

    added repeated_range example updated strided_range example

--- examples/strided_range.cu
+++ examples/strided_range.cu
@@ -21,9 +21,9 @@ class strided_range
 
     struct stride_functor : public thrust::unary_function<difference_type,difference_type>
     {
-        difference_type stride;
+        int stride;
 
-        stride_functor(difference_type stride)
+        stride_functor(int stride)
             : stride(stride) {}
 
         __host__ __device__
@@ -37,27 +37,27 @@ class strided_range
     typedef typename thrust::transform_iterator<stride_functor, CountingIterator> TransformIterator;
     typedef typename thrust::permutation_iterator<Iterator,TransformIterator>     PermutationIterator;
 
+    // type of the strided_range iterator
+    typedef PermutationIterator iterator;
 
-
-
+    // construct strided_range for the range [first,last)
     strided_range(Iterator first, Iterator last, int stride)
         : first(first), last(last), stride(stride) {}
    
-    PermutationIterator begin(void) const
+    iterator begin(void) const
     {
         return PermutationIterator(first, TransformIterator(CountingIterator(0), stride_functor(stride)));
     }
 
-    PermutationIterator end(void) const
+    iterator end(void) const
     {
         return begin() + ((last - first) + (stride - 1)) / stride;
     }
     
     protected:
-    difference_type stride;
+    int stride;
     Iterator first;
     Iterator last;
-    
 };
 
 int main(void)
