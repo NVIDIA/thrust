39e7d72de732f068266aafbd399aef616aa31e85
Jared Hoberock <jaredhoberock@gmail.com>
2010-09-03 14:02:40 -0700

    Changed my mind; give Thrust-specific names to the cudaError enum values to match N3092 & Boost's convention. Add system/detail/errno.h to provide portable POSIX error codes.

--- thrust/system/cuda_error.h
+++ thrust/system/cuda_error.h
@@ -46,8 +46,51 @@ namespace system
 namespace cuda_errc
 {
 
-// typedef cudaError_t to cuda_errc_t so we don't have to update the list
-typedef cudaError_t cuda_errc_t;
+enum cuda_errc_t
+{
+  // from cuda/include/driver_types.h
+  success                      = ::cudaSuccess,
+  missing_configuration        = ::cudaErrorMissingConfiguration,
+  memory_allocation            = ::cudaErrorMemoryAllocation,
+  initialization_error         = ::cudaErrorInitializationError,
+  launch_failure               = ::cudaErrorLaunchFailure,
+  prior_launch_failure         = ::cudaErrorPriorLaunchFailure,
+  launch_timeout               = ::cudaErrorLaunchTimeout,
+  launch_out_of_resources      = ::cudaErrorLaunchOutOfResources,
+  invalid_device_function      = ::cudaErrorInvalidDeviceFunction,
+  invalid_configuration        = ::cudaErrorInvalidConfiguration,
+  invalid_device               = ::cudaErrorInvalidDevice,
+  invalid_value                = ::cudaErrorInvalidValue,
+  invalid_pitch_value          = ::cudaErrorInvalidPitchValue,
+  invalid_symbol               = ::cudaErrorInvalidSymbol,
+  map_buffer_object_failed     = ::cudaErrorMapBufferObjectFailed,
+  unmap_buffer_object_failed   = ::cudaErrorUnmapBufferObjectFailed,
+  invalid_host_pointer         = ::cudaErrorInvalidHostPointer,
+  invalid_device_pointer       = ::cudaErrorInvalidDevicePointer,
+  invalid_texture              = ::cudaErrorInvalidTexture,
+  invalid_texture_binding      = ::cudaErrorInvalidTextureBinding,
+  invalid_channel_descriptor   = ::cudaErrorInvalidChannelDescriptor,
+  invalid_memcpy_direction     = ::cudaErrorInvalidMemcpyDirection,
+  address_of_constant_error    = ::cudaErrorAddressOfConstant,
+  texture_fetch_failed         = ::cudaErrorTextureFetchFailed,
+  texture_not_bound            = ::cudaErrorTextureNotBound,
+  synchronization_error        = ::cudaErrorSynchronizationError,
+  invalid_filter_setting       = ::cudaErrorInvalidFilterSetting,
+  invalid_norm_setting         = ::cudaErrorInvalidNormSetting,
+  mixed_device_execution       = ::cudaErrorMixedDeviceExecution,
+  cuda_runtime_unloading       = ::cudaErrorCudartUnloading,
+  unknown                      = ::cudaErrorUnknown,
+  not_yet_implemented          = ::cudaErrorNotYetImplemented,
+  memory_value_too_large       = ::cudaErrorMemoryValueTooLarge,
+  invalid_resource_handle      = ::cudaErrorInvalidResourceHandle,
+  not_ready                    = ::cudaErrorNotReady,
+  insufficient_driver          = ::cudaErrorInsufficientDriver,
+  set_on_active_process_error  = ::cudaErrorSetOnActiveProcess,
+  no_device                    = ::cudaErrorNoDevice,
+  ecc_uncorrectable            = ::cudaErrorECCUncorrectable,
+  startup_failure              = ::cudaErrorStartupFailure,
+}; // end cuda_errc_t
+
 
 } // end namespace cuda_errc
 
