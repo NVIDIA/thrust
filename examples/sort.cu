90d8ee8215f3da8f88b8f11370bf95c1d0405373
Nathan Bell <wnbell@gmail.com>
2010-12-17 23:24:37 -0500

    expanded sort example
    P4 Change 8279477 on 2010/12/17 23:24:37 by nbell

--- examples/sort.cu
+++ examples/sort.cu
@@ -1,20 +1,164 @@
-#include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
-#include <thrust/generate.h>
 #include <thrust/sort.h>
-#include <cstdlib>
+#include <thrust/random.h>
+#include <iostream>
+#include <iomanip>
+
+// Helper routines
+
+void initialize(thrust::device_vector<int>& v)
+{
+  thrust::default_random_engine rng(12345);
+  thrust::uniform_int_distribution<int> dist(10, 99);
+  for(size_t i = 0; i < v.size(); i++)
+    v[i] = dist(rng);
+}
+
+void initialize(thrust::device_vector<float>& v)
+{
+  thrust::default_random_engine rng(12345);
+  thrust::uniform_int_distribution<int> dist(2, 19);
+  for(size_t i = 0; i < v.size(); i++)
+    v[i] = dist(rng) / 2.0f;
+}
+
+void initialize(thrust::device_vector< thrust::pair<int,int> >& v)
+{
+  thrust::default_random_engine rng(12345);
+  thrust::uniform_int_distribution<int> dist(0,9);
+  for(size_t i = 0; i < v.size(); i++)
+    v[i] = thrust::make_pair(dist(rng),dist(rng));
+}
+
+void initialize(thrust::device_vector<int>& v1, thrust::device_vector<int>& v2)
+{
+  thrust::default_random_engine rng(12345);
+  thrust::uniform_int_distribution<int> dist(10, 99);
+  for(size_t i = 0; i < v1.size(); i++)
+  {
+    v1[i] = dist(rng);
+    v2[i] = i;
+  }
+}
+
+void print(const thrust::device_vector<int>& v)
+{
+  for(size_t i = 0; i < v.size(); i++)
+    std::cout << " " << v[i];
+  std::cout << "\n";
+}
+
+void print(const thrust::device_vector<float>& v)
+{
+  for(size_t i = 0; i < v.size(); i++)
+    std::cout << " " << std::fixed << std::setprecision(1) << v[i];
+  std::cout << "\n";
+}
+
+void print(const thrust::device_vector< thrust::pair<int,int> >& v)
+{
+  for(size_t i = 0; i < v.size(); i++)
+  {
+    thrust::pair<int,int> p = v[i];
+    std::cout << " (" << p.first << "," << p.second << ")";
+  }
+  std::cout << "\n";
+}
+
+void print(thrust::device_vector<int>& v1, thrust::device_vector<int> v2)
+{
+  for(size_t i = 0; i < v1.size(); i++)
+    std::cout << " (" << v1[i] << "," << std::setw(2) << v2[i] << ")";
+  std::cout << "\n";
+}
+
+
+// user-defined comparison operator that acts like less<int>,
+// except even numbers are considered to be smaller than odd numbers
+struct evens_before_odds
+{
+  __host__ __device__
+  bool operator()(int x, int y)
+  {
+    if (x % 2 == y % 2)
+      return x < y;
+    else if (x % 2)
+      return false;
+    else
+      return true;
+  }
+};
+
 
 int main(void)
 {
-  // generate random data on the host
-  thrust::host_vector<int> h_vec(20);
-  thrust::generate(h_vec.begin(), h_vec.end(), rand);
+  size_t N = 16;
 
-  // transfer to device
-  thrust::device_vector<int> d_vec = h_vec;
+  std::cout << "sorting integers\n";
+  {
+    thrust::device_vector<int> keys(N);
+    initialize(keys);
+    print(keys);
+    thrust::sort(keys.begin(), keys.end());
+    print(keys);
+  }
+  
+  std::cout << "\nsorting integers (descending)\n";
+  {
+    thrust::device_vector<int> keys(N);
+    initialize(keys);
+    print(keys);
+    thrust::sort(keys.begin(), keys.end(), thrust::greater<int>());
+    print(keys);
+  }
+  
+  std::cout << "\nsorting integers (user-defined comparison)\n";
+  {
+    thrust::device_vector<int> keys(N);
+    initialize(keys);
+    print(keys);
+    thrust::sort(keys.begin(), keys.end(), evens_before_odds());
+    print(keys);
+  }
 
-  // sort on device
-  thrust::sort(d_vec.begin(), d_vec.end());
+  std::cout << "\nsorting floats\n";
+  {
+    thrust::device_vector<float> keys(N);
+    initialize(keys);
+    print(keys);
+    thrust::sort(keys.begin(), keys.end());
+    print(keys);
+  }
+  
+  std::cout << "\nsorting pairs\n";
+  {
+    thrust::device_vector< thrust::pair<int,int> > keys(N);
+    initialize(keys);
+    print(keys);
+    thrust::sort(keys.begin(), keys.end());
+    print(keys);
+  }
+  
+  std::cout << "\nkey-value sorting\n";
+  {
+    thrust::device_vector<int> keys(N);
+    thrust::device_vector<int> values(N);
+    initialize(keys, values);
+    print(keys, values);
+    thrust::sort_by_key(keys.begin(), keys.end(), values.begin());
+    print(keys, values);
+  }
+  
+  std::cout << "\nkey-value sorting (descending)\n";
+  {
+    thrust::device_vector<int> keys(N);
+    thrust::device_vector<int> values(N);
+    initialize(keys, values);
+    print(keys, values);
+    thrust::sort_by_key(keys.begin(), keys.end(), values.begin(), thrust::greater<int>());
+    print(keys, values);
+  }
 
   return 0;
 }
+
