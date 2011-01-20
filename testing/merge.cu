0ccaac000d2ee83e1b7f8362e9df6beac12e7ffb
Nathan Bell <wnbell@gmail.com>
2011-01-20 10:00:10 -0500

    divided merge, set_difference, set_intersection, and set_union tests into multiple files
    P4 Change 8545807 on 2011/01/20 10:00:10 by nbell

--- testing/merge.cu
+++ testing/merge.cu
@@ -66,48 +66,6 @@ template<typename T>
 DECLARE_VARIABLE_UNITTEST(TestMerge);
 
 
-template<typename U>
-  void TestMergeKeyValue(size_t n)
-{
-  typedef key_value<U,U> T;
-
-  thrust::host_vector<U> h_keys_a   = unittest::random_integers<U>(n);
-  thrust::host_vector<U> h_values_a = unittest::random_integers<U>(n);
-
-  thrust::host_vector<U> h_keys_b   = unittest::random_integers<U>(n);
-  thrust::host_vector<U> h_values_b = unittest::random_integers<U>(n);
-
-  thrust::host_vector<T> h_a(n), h_b(n);
-  for(size_t i = 0; i < n; ++i)
-  {
-    h_a[i] = T(h_keys_a[i], h_values_a[i]);
-    h_b[i] = T(h_keys_b[i], h_values_b[i]);
-  }
-
-  thrust::stable_sort(h_a.begin(), h_a.end());
-  thrust::stable_sort(h_b.begin(), h_b.end());
-
-  thrust::device_vector<T> d_a = h_a;
-  thrust::device_vector<T> d_b = h_b;
-
-  thrust::host_vector<T>   h_result(h_a.size() + h_b.size());
-  thrust::device_vector<T> d_result(d_a.size() + d_b.size());
-
-  typename thrust::host_vector<T>::iterator   h_end;
-  typename thrust::device_vector<T>::iterator d_end;
-  
-  h_end = thrust::merge(h_a.begin(), h_a.end(),
-                        h_b.begin(), h_b.end(),
-                        h_result.begin());
-
-  d_end = thrust::merge(d_a.begin(), d_a.end(),
-                        d_b.begin(), d_b.end(),
-                        d_result.begin());
-
-  ASSERT_EQUAL_QUIET(h_result, d_result);
-}
-DECLARE_VARIABLE_UNITTEST(TestMergeKeyValue);
-
 template<typename T>
   void TestMergeAscending(size_t n)
 {
@@ -139,49 +97,4 @@ template<typename T>
   ASSERT_EQUAL(h_result, d_result);
 }
 DECLARE_VARIABLE_UNITTEST(TestMergeAscending);
-
-
-template<typename U>
-  void TestMergeKeyValueAscending(size_t n)
-{
-  typedef key_value<U,U> T;
-
-  thrust::host_vector<U> h_keys_a   = unittest::random_integers<U>(n);
-  thrust::host_vector<U> h_values_a = unittest::random_integers<U>(n);
-
-  thrust::host_vector<U> h_keys_b   = unittest::random_integers<U>(n);
-  thrust::host_vector<U> h_values_b = unittest::random_integers<U>(n);
-
-  thrust::host_vector<T> h_a(n), h_b(n);
-  for(size_t i = 0; i < n; ++i)
-  {
-    h_a[i] = T(h_keys_a[i], h_values_a[i]);
-    h_b[i] = T(h_keys_b[i], h_values_b[i]);
-  }
-
-  thrust::stable_sort(h_a.begin(), h_a.end(), thrust::greater<T>());
-  thrust::stable_sort(h_b.begin(), h_b.end(), thrust::greater<T>());
-
-  thrust::device_vector<T> d_a = h_a;
-  thrust::device_vector<T> d_b = h_b;
-
-  thrust::host_vector<T>   h_result(h_a.size() + h_b.size());
-  thrust::device_vector<T> d_result(d_a.size() + d_b.size());
-
-  typename thrust::host_vector<T>::iterator   h_end;
-  typename thrust::device_vector<T>::iterator d_end;
-  
-  h_end = thrust::merge(h_a.begin(), h_a.end(),
-                        h_b.begin(), h_b.end(),
-                        h_result.begin(),
-                        thrust::greater<T>());
-
-  d_end = thrust::merge(d_a.begin(), d_a.end(),
-                        d_b.begin(), d_b.end(),
-                        d_result.begin(),
-                        thrust::greater<T>());
-
-  ASSERT_EQUAL_QUIET(h_result, d_result);
-}
-DECLARE_VARIABLE_UNITTEST(TestMergeKeyValueAscending);
 
--- testing/set_union.cu
+++ testing/set_union.cu
@@ -65,7 +65,7 @@ void TestSetUnion(const size_t n)
   thrust::device_vector<T> d_a = h_a;
   thrust::device_vector<T> d_b = h_b;
 
-  thrust::host_vector<T> h_result(h_a.size() + h_b.size());
+  thrust::host_vector<T>   h_result(h_a.size() + h_b.size());
   thrust::device_vector<T> d_result(d_a.size() + d_b.size());
 
   typename thrust::host_vector<T>::iterator h_end;
@@ -86,51 +86,6 @@ void TestSetUnion(const size_t n)
 DECLARE_VARIABLE_UNITTEST(TestSetUnion);
 
 
-template<typename U>
-  void TestSetUnionKeyValue(size_t n)
-{
-  typedef key_value<U,U> T;
-
-  thrust::host_vector<U> h_keys_a   = unittest::random_integers<U>(n);
-  thrust::host_vector<U> h_values_a = unittest::random_integers<U>(n);
-
-  thrust::host_vector<U> h_keys_b   = unittest::random_integers<U>(n);
-  thrust::host_vector<U> h_values_b = unittest::random_integers<U>(n);
-
-  thrust::host_vector<T> h_a(n), h_b(n);
-  for(size_t i = 0; i < n; ++i)
-  {
-    h_a[i] = T(h_keys_a[i], h_values_a[i]);
-    h_b[i] = T(h_keys_b[i], h_values_b[i]);
-  }
-
-  thrust::stable_sort(h_a.begin(), h_a.end());
-  thrust::stable_sort(h_b.begin(), h_b.end());
-
-  thrust::device_vector<T> d_a = h_a;
-  thrust::device_vector<T> d_b = h_b;
-
-  thrust::host_vector<T>   h_result(h_a.size() + h_b.size());
-  thrust::device_vector<T> d_result(d_a.size() + d_b.size());
-
-  typename thrust::host_vector<T>::iterator   h_end;
-  typename thrust::device_vector<T>::iterator d_end;
-  
-  h_end = thrust::set_union(h_a.begin(), h_a.end(),
-                            h_b.begin(), h_b.end(),
-                            h_result.begin());
-  h_result.erase(h_end, h_result.end());
-
-  d_end = thrust::set_union(d_a.begin(), d_a.end(),
-                            d_b.begin(), d_b.end(),
-                            d_result.begin());
-  d_result.erase(d_end, d_result.end());
-
-  ASSERT_EQUAL_QUIET(h_result, d_result);
-}
-DECLARE_VARIABLE_UNITTEST(TestSetUnionKeyValue);
-
-
 template<typename T>
   void TestSetUnionDescending(size_t n)
 {
@@ -143,7 +98,7 @@ template<typename T>
   thrust::device_vector<T> d_a = h_a;
   thrust::device_vector<T> d_b = h_b;
 
-  thrust::host_vector<T> h_result(h_a.size() + h_b.size());
+  thrust::host_vector<T>   h_result(h_a.size() + h_b.size());
   thrust::device_vector<T> d_result(d_a.size() + d_b.size());
 
   typename thrust::host_vector<T>::iterator h_end;
@@ -164,51 +119,4 @@ template<typename T>
   ASSERT_EQUAL(h_result, d_result);
 }
 DECLARE_VARIABLE_UNITTEST(TestSetUnionDescending);
-
-
-template<typename U>
-  void TestSetUnionKeyValueDescending(size_t n)
-{
-  typedef key_value<U,U> T;
-
-  thrust::host_vector<U> h_keys_a   = unittest::random_integers<U>(n);
-  thrust::host_vector<U> h_values_a = unittest::random_integers<U>(n);
-
-  thrust::host_vector<U> h_keys_b   = unittest::random_integers<U>(n);
-  thrust::host_vector<U> h_values_b = unittest::random_integers<U>(n);
-
-  thrust::host_vector<T> h_a(n), h_b(n);
-  for(size_t i = 0; i < n; ++i)
-  {
-    h_a[i] = T(h_keys_a[i], h_values_a[i]);
-    h_b[i] = T(h_keys_b[i], h_values_b[i]);
-  }
-
-  thrust::stable_sort(h_a.begin(), h_a.end(), thrust::greater<T>());
-  thrust::stable_sort(h_b.begin(), h_b.end(), thrust::greater<T>());
-
-  thrust::device_vector<T> d_a = h_a;
-  thrust::device_vector<T> d_b = h_b;
-
-  thrust::host_vector<T>   h_result(h_a.size() + h_b.size());
-  thrust::device_vector<T> d_result(d_a.size() + d_b.size());
-
-  typename thrust::host_vector<T>::iterator   h_end;
-  typename thrust::device_vector<T>::iterator d_end;
-  
-  h_end = thrust::set_union(h_a.begin(), h_a.end(),
-                            h_b.begin(), h_b.end(),
-                            h_result.begin(),
-                            thrust::greater<T>());
-  h_result.erase(h_end, h_result.end());
-
-  d_end = thrust::set_union(d_a.begin(), d_a.end(),
-                            d_b.begin(), d_b.end(),
-                            d_result.begin(),
-                            thrust::greater<T>());
-  d_result.erase(d_end, d_result.end());
-
-  ASSERT_EQUAL_QUIET(h_result, d_result);
-}
-DECLARE_VARIABLE_UNITTEST(TestSetUnionKeyValueDescending);
 
