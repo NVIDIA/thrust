0df168c364f9eaa2517c160220de37c1fb2b30da
Nathan Bell <wnbell@gmail.com>
2011-07-20 10:16:08 -0400

    test radix sort optimization on both host and device
    P4 Change 10491144 on 2011/07/20 10:16:08 by nbell

--- testing/sort_variable_bits.cu
+++ testing/sort_variable_bits.cu
@@ -1,12 +1,7 @@
 #include <unittest/unittest.h>
-#include <thrust/functional.h>
-#include <thrust/sequence.h>
-#include <thrust/device_malloc_allocator.h>
-
 #include <thrust/sort.h>
-#include <thrust/detail/backend/cuda/detail/stable_radix_sort.h>
 
-#if THRUST_DEVICE_BACKEND == THRUST_DEVICE_BACKEND_CUDA
+#include <algorithm>
 
 using namespace unittest;
 
@@ -21,27 +16,30 @@ typedef unittest::type_list<
                             unsigned long long> UnsignedIntegerTypes;
 
 template <typename T>
-struct TestRadixSortVariableBits
+struct TestSortVariableBits
 {
   void operator()(const size_t n)
   {
     for(size_t num_bits = 0; num_bits < 8 * sizeof(T); num_bits += 3){
+
         thrust::host_vector<T>  h_keys = unittest::random_integers<T>(n);
    
         size_t mask = (1 << num_bits) - 1;
         for(size_t i = 0; i < n; i++)
             h_keys[i] &= mask;
 
-        thrust::device_vector<T> d_keys = h_keys;
+        thrust::host_vector<T>   reference = h_keys;
+        thrust::device_vector<T> d_keys    = h_keys;
     
-        thrust::stable_sort(h_keys.begin(), h_keys.end());
-        thrust::detail::backend::cuda::detail::stable_radix_sort(d_keys.begin(), d_keys.end());
+        std::sort(reference.begin(), reference.end());
+
+        thrust::sort(h_keys.begin(), h_keys.end());
+        thrust::sort(d_keys.begin(), d_keys.end());
     
-        ASSERT_ALMOST_EQUAL(h_keys, d_keys);
+        ASSERT_EQUAL(reference, h_keys);
+        ASSERT_EQUAL(h_keys, d_keys);
     }
   }
 };
-VariableUnitTest<TestRadixSortVariableBits, UnsignedIntegerTypes> TestRadixSortVariableBitsInstance;
-
-#endif // THRUST_DEVICE_BACKEND == THRUST_DEVICE_BACKEND_CUDA
+VariableUnitTest<TestSortVariableBits, UnsignedIntegerTypes> TestSortVariableBitsInstance;
 
