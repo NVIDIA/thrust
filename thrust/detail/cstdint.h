68ec93b93a7639736b69450800ceb3912ef830ef
Nathan Bell <wnbell@gmail.com>
2011-07-20 22:23:17 -0700

    expanded /detail/cstdint.h and added basic testing
    P4 Change 10494737 on 2011/07/20 22:23:17 by nbell

--- thrust/detail/cstdint.h
+++ thrust/detail/cstdint.h
@@ -16,55 +16,64 @@
 
 #pragma once
 
+#if (THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_GCC)
+#include <stdint.h>
+#endif
+
 namespace thrust
 {
-
 namespace detail
 {
 
-// an oracle to tell us how to define uint64_t
-template<int word_size = sizeof(int*)> struct divine_uint64_t;
+#if (THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_MSVC)
 
-// 32b machine type
-template<>
-  struct divine_uint64_t<4>
-{
-  typedef unsigned long long int type;
-};
+#if (_MSC_VER < 1300)
+   typedef signed   char     int8_t;
+   typedef signed   short    int16_t;
+   typedef signed   int      int32_t;
+   typedef unsigned char     uint8_t;
+   typedef unsigned short    uint16_t;
+   typedef unsigned int      uint32_t;
+#else
+   typedef signed   __int8   int8_t;
+   typedef signed   __int16  int16_t;
+   typedef signed   __int32  int32_t;
+   typedef unsigned __int8   uint8_t;
+   typedef unsigned __int16  uint16_t;
+   typedef unsigned __int32  uint32_t;
+#endif
+typedef signed   __int64     int64_t;
+typedef unsigned __int64     uint64_t;
 
-// 64b machine type
-template<>
-  struct divine_uint64_t<8>
-{
-  typedef unsigned long long int type;
-};
+#else
 
-typedef unsigned int            uint32_t;
-typedef divine_uint64_t<>::type uint64_t;
+typedef ::int8_t   int8_t;
+typedef ::int16_t  int16_t;
+typedef ::int32_t  int32_t;
+typedef ::int64_t  int64_t;
+typedef ::uint8_t  uint8_t;
+typedef ::uint16_t uint16_t;
+typedef ::uint32_t uint32_t;
+typedef ::uint64_t uint64_t;
+
+#endif
 
 
 // an oracle to tell us how to define intptr_t
-template<int word_size = sizeof(int*)> struct divine_intptr_t;
+template<int word_size = sizeof(void*)> struct divine_intptr_t;
+template<int word_size = sizeof(void*)> struct divine_uintptr_t;
 
-// use uint32_t on 32b platforms
-template<>
-  struct divine_intptr_t<4>
-{
-  typedef thrust::detail::uint32_t type;
-};
+// 32b platforms
+template<>  struct divine_intptr_t<4>  {  typedef thrust::detail::int32_t  type; };
+template<>  struct divine_uintptr_t<4> {  typedef thrust::detail::uint32_t type; };
 
-// use uint64_t on 64b platforms
-template<>
-  struct divine_intptr_t<8>
-{
-  typedef thrust::detail::uint64_t type;
-};
+// 64b platforms
+template<>  struct divine_intptr_t<8>  { typedef thrust::detail::int64_t  type; };
+template<>  struct divine_uintptr_t<8> { typedef thrust::detail::uint64_t type; };
 
 typedef divine_intptr_t<>::type   intptr_t;
-typedef thrust::detail::intptr_t uintptr_t;
-
+typedef divine_uintptr_t<>::type  uintptr_t;
 
 } // end detail
-
 } // end thrust
 
