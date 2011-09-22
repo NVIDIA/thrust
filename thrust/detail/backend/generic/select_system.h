2879a5e81d7bfeaf26acd2ebff03b7f76ce1cd57
Jared Hoberock <jaredhoberock@gmail.com>
2011-09-22 16:45:10 -0700

    Add stubs for generic::select_system.

---
+++ thrust/detail/backend/generic/select_system.h
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2008-2011 NVIDIA Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#pragma once
+
+namespace thrust
+{
+namespace detail
+{
+namespace backend
+{
+namespace generic
+{
+
+template<typename Tag>
+  Tag select_system(Tag)
+{
+  return Tag();
+} // end select_system()
+
+// XXX this is just a placeholder
+//     for now, return Tag1
+template<typename Tag1, typename Tag2>
+  Tag1 select_system(Tag1, Tag2)
+{
+  return Tag1();
+} // end select_system()
+
+} // end generic
+} // end backend
+} // end detail
+} // end thrust
+
