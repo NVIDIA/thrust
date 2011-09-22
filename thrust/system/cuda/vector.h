9a77b6542770071eb67aefde68ea90d531ac1253
Jared Hoberock <jaredhoberock@gmail.com>
2011-09-21 17:29:45 -0700

    Introduce <backend>::vector for each backend.

---
+++ thrust/system/cuda/vector.h
@@ -0,0 +1,86 @@
+/*
+ *  Copyright 2008-2011 NVIDIA Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in ccudaliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#pragma once
+
+#include <thrust/detail/config.h>
+#include <thrust/system/cuda/memory.h>
+#include <thrust/detail/vector_base.h>
+#include <vector>
+
+namespace thrust
+{
+
+// forward declaration of host_vector
+template<typename T, typename Allocator> class host_vector;
+
+namespace system
+{
+namespace cuda
+{
+
+// XXX upon c++11
+// template<typename T, typename Allocator = allocator<T> > using vector = thrust::detail::vector_base<T,Allocator>;
+
+template<typename T, typename Allocator = allocator<T> >
+  class vector
+    : public thrust::detail::vector_base<T,Allocator>
+{
+  private:
+    typedef thrust::detail::vector_base<T,Allocator> super_t;
+
+  public:
+    typedef typename super_t::size_type  size_type;
+    typedef typename super_t::value_type value_type;
+
+    vector();
+
+    explicit vector(size_type n, const value_type &value = value_type());
+
+    vector(const vector &x);
+
+    template<typename OtherT, typename OtherAllocator>
+    vector(const thrust::detail::vector_base<OtherT,OtherAllocator> &x);
+
+    template<typename OtherT, typename OtherAllocator>
+    vector(const std::vector<OtherT,OtherAllocator> &x);
+
+    template<typename InputIterator>
+    vector(InputIterator first, InputIterator last);
+
+    // XXX vector_base should take a Derived type so we don't have to define these superfluous assigns
+    template<typename OtherT, typename OtherAllocator>
+    vector &operator=(const std::vector<OtherT,OtherAllocator> &x);
+
+    template<typename OtherT, typename OtherAllocator>
+    vector &operator=(const thrust::detail::vector_base<OtherT,OtherAllocator> &x);
+}; // end vector
+
+} // end cuda
+} // end system
+
+// alias system::cuda names at top-level
+namespace cuda
+{
+
+using thrust::system::cuda::vector;
+
+} // end cuda
+
+} // end thrust
+
+#include <thrust/system/cuda/detail/vector.inl>
+
