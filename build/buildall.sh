bdf961b2db4e0c5dae4799cfe39cbe780ba5dd84
Jared Hoberock <jaredhoberock@gmail.com>
2012-02-22 17:25:34 -0800

    Build trivial tests. Only do the full cross product for examples and trivial tests. Build and run the unit tests for each device system using cpp as the host system

--- build/buildall.sh
+++ build/buildall.sh
@@ -6,21 +6,24 @@ THRUST="`( cd \"$THRUST\"/.. && pwd )`"  # absolutized and normalized
 THRUST_BUILD=$THRUST/build
 THRUST_EXAMPLES=$THRUST/examples
 THRUST_TESTING=$THRUST/testing
+THRUST_TRIVIAL_TESTS=$THRUST/testing/trivial_tests
 
 cpu_host_systems="cpp omp tbb" 
 cpu_device_systems="omp tbb"
+device_systems="cuda omp tbb"
 
+num_gpus=`ls /proc/driver/nvidia/gpus/ | wc -w`
+
+# build the full product of (host_backend,backend) for examples and trivial tests
 for host in $cpu_host_systems; do
     for device in $cpu_device_systems; do
         cd $THRUST_EXAMPLES
         scons -j2 host_backend=$host backend=$device run_examples
-        cd $THRUST_TESTING
+        cd $THRUST_TRIVIAL_TESTS
         scons -j2 host_backend=$host backend=$device
         ./tester
     done
 done
-
-num_gpus=`ls /proc/driver/nvidia/gpus/ | wc -w`
 
 if (( num_gpus > 0)); then
   nvcc $THRUST_BUILD/print_sm_version.cpp -o /tmp/print_sm_version
@@ -29,10 +32,28 @@ if (( num_gpus > 0)); then
           arch=`/tmp/print_sm_version $device_id`
           cd $THRUST_EXAMPLES
           scons -j2 host_backend=$host backend=cuda arch=$arch run_examples
-          cd $THRUST_TESTING
-          scons -j2 host_backend=$host backend=cuda arch=$arch
-          ./tester --device=$device_id
+          cd $THRUST_TRIVIAL_TESTS
+          scons -j2 host_backend=$host backend=$device
+          ./tester
       done
+  done
+fi
+
+
+# build unit tests for each cpu device systems
+for device in $cpu_device_systems; do
+    cd $THRUST_TESTING
+    scons -j2 host_backend=cpp backend=$device
+    ./tester
+done
+
+if (( num_gpus > 0)); then
+  nvcc $THRUST_BUILD/print_sm_version.cpp -o /tmp/print_sm_version
+  for ((device_id=0;device_id<num_gpus;device_id++)); do
+      arch=`/tmp/print_sm_version $device_id`
+      cd $THRUST_TESTING
+      scons -j2 host_backend=cpp backend=cuda
+      ./tester
   done
 fi
 
