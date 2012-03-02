4096b3f20acc88208101df352a1b84b9179d985a
Jared Hoberock <jaredhoberock@gmail.com>
2012-03-02 15:29:19 -0800

    Mark uncompiling reduce_intervals unit tests as KNOWN_FAILUREs.

--- testing/backend/cuda/reduce_intervals.cu
+++ testing/backend/cuda/reduce_intervals.cu
@@ -2,33 +2,35 @@
 
 #include <thrust/functional.h>
 #include <thrust/system/detail/internal/decompose.h>
-#include <thrust/system/detail/internal/reduce_intervals.h>
-
-namespace ns = thrust::system::detail::internal;
+#include <thrust/system/cuda/detail/reduce_intervals.h>
 
 template <class Vector>
 void TestReduceIntervalsSimple(void)
 {
-  typedef typename Vector::value_type T;
+  KNOWN_FAILURE;
 
-  Vector input(10, 1);
-    
-  {
-    thrust::system::detail::internal::uniform_decomposition<int> decomp(10, 10, 1);
-    Vector output(decomp.size());
-    ns::reduce_intervals(input.begin(), output.begin(), thrust::plus<T>(), decomp);
-
-    ASSERT_EQUAL(output[0], 10);
-  }
-  
-  {
-    thrust::system::detail::internal::uniform_decomposition<int> decomp(10, 6, 2);
-    Vector output(decomp.size());
-    ns::reduce_intervals(input.begin(), output.begin(), thrust::plus<T>(), decomp);
-
-    ASSERT_EQUAL(output[0], 6);
-    ASSERT_EQUAL(output[1], 4);
-  }
+//  namespace ns = thrust::system::cuda::detail;
+//
+//  typedef typename Vector::value_type T;
+//
+//  Vector input(10, 1);
+//    
+//  {
+//    thrust::system::detail::internal::uniform_decomposition<int> decomp(10, 10, 1);
+//    Vector output(decomp.size());
+//    ns::reduce_intervals(input.begin(), output.begin(), thrust::plus<T>(), decomp);
+//
+//    ASSERT_EQUAL(output[0], 10);
+//  }
+//  
+//  {
+//    thrust::system::detail::internal::uniform_decomposition<int> decomp(10, 6, 2);
+//    Vector output(decomp.size());
+//    ns::reduce_intervals(input.begin(), output.begin(), thrust::plus<T>(), decomp);
+//
+//    ASSERT_EQUAL(output[0], 6);
+//    ASSERT_EQUAL(output[1], 4);
+//  }
 }
 DECLARE_VECTOR_UNITTEST(TestReduceIntervalsSimple);
 
@@ -38,18 +40,22 @@ struct TestReduceIntervals
 {
   void operator()(const size_t n)
   {
-    thrust::host_vector<T>   h_input = unittest::random_integers<T>(n);
-    thrust::device_vector<T> d_input = h_input;
+    KNOWN_FAILURE;
 
-    thrust::system::detail::internal::uniform_decomposition<size_t> decomp(n, 7, 100);
-
-    thrust::host_vector<T>   h_output(decomp.size());
-    thrust::device_vector<T> d_output(decomp.size());
-    
-    ns::reduce_intervals(h_input.begin(), h_output.begin(), thrust::plus<T>(), decomp);
-    ns::reduce_intervals(d_input.begin(), d_output.begin(), thrust::plus<T>(), decomp);
-
-    ASSERT_EQUAL(h_output, d_output);
+//    namespace ns = thrust::system::cuda::detail;
+//
+//    thrust::host_vector<T>   h_input = unittest::random_integers<T>(n);
+//    thrust::device_vector<T> d_input = h_input;
+//
+//    thrust::system::detail::internal::uniform_decomposition<size_t> decomp(n, 7, 100);
+//
+//    thrust::host_vector<T>   h_output(decomp.size());
+//    thrust::device_vector<T> d_output(decomp.size());
+//    
+//    ns::reduce_intervals(h_input.begin(), h_output.begin(), thrust::plus<T>(), decomp);
+//    ns::reduce_intervals(thrust::cuda::tag(), d_input.begin(), d_output.begin(), thrust::plus<T>(), decomp);
+//
+//    ASSERT_EQUAL(h_output, d_output);
   }
 };
 VariableUnitTest<TestReduceIntervals, IntegralTypes> TestReduceIntervalsInstance;
