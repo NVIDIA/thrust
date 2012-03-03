f203f4f51d0ce0cdcdfbb98e79deaa880c73e3a9
Nathan Bell <wnbell@gmail.com>
2012-03-03 15:20:12 -0500

    restored reduce_intervals unit tests

--- testing/backend/omp/reduce_intervals.cu
+++ testing/backend/omp/reduce_intervals.cu
@@ -4,59 +4,99 @@
 #include <thrust/system/detail/internal/decompose.h>
 #include <thrust/system/omp/detail/reduce_intervals.h>
 
-template <class Vector>
-void TestReduceIntervalsSimple(void)
+// CPP reference implementation 
+template <typename InputIterator,
+          typename OutputIterator,
+          typename BinaryFunction,
+          typename Decomposition>
+void reduce_intervals(InputIterator input,
+                      OutputIterator output,
+                      BinaryFunction binary_op,
+                      Decomposition decomp)
 {
-  KNOWN_FAILURE;
+  typedef typename thrust::iterator_value<OutputIterator>::type OutputType;
+  typedef typename Decomposition::index_type index_type;
 
-//  namespace ns = thrust::system::omp::detail;
-//
-//  typedef typename Vector::value_type T;
-//
-//  Vector input(10, 1);
-//    
-//  {
-//    thrust::system::detail::internal::uniform_decomposition<int> decomp(10, 10, 1);
-//    Vector output(decomp.size());
-//    ns::reduce_intervals(input.begin(), output.begin(), thrust::plus<T>(), decomp);
-//
-//    ASSERT_EQUAL(output[0], 10);
-//  }
-//  
-//  {
-//    thrust::system::detail::internal::uniform_decomposition<int> decomp(10, 6, 2);
-//    Vector output(decomp.size());
-//    ns::reduce_intervals(input.begin(), output.begin(), thrust::plus<T>(), decomp);
-//
-//    ASSERT_EQUAL(output[0], 6);
-//    ASSERT_EQUAL(output[1], 4);
-//  }
+  // wrap binary_op
+  thrust::detail::host_function<
+    BinaryFunction,
+    OutputType
+  > wrapped_binary_op(binary_op);
+
+  for(index_type i = 0; i < decomp.size(); ++i, ++output)
+  {
+    InputIterator begin = input + decomp[i].begin();
+    InputIterator end   = input + decomp[i].end();
+
+    if (begin != end)
+    {
+      OutputType sum = *begin;
+
+      ++begin;
+
+      while (begin != end)
+      {
+        sum = wrapped_binary_op(sum, *begin);
+        ++begin;
+      }
+
+      *output = sum;
+    }
+  }
 }
-DECLARE_VECTOR_UNITTEST(TestReduceIntervalsSimple);
+
+
+void TestOmpReduceIntervalsSimple(void)
+{
+  typedef int T;
+  typedef thrust::device_vector<T> Vector;
+
+  using thrust::system::omp::detail::reduce_intervals;
+  using thrust::system::detail::internal::uniform_decomposition;
+
+  Vector input(10, 1);
+    
+  {
+    uniform_decomposition<int> decomp(10, 10, 1);
+    Vector output(decomp.size());
+    reduce_intervals(thrust::system::omp::tag(), input.begin(), output.begin(), thrust::plus<T>(), decomp);
+
+    ASSERT_EQUAL(output[0], 10);
+  }
+  
+  {
+    uniform_decomposition<int> decomp(10, 6, 2);
+    Vector output(decomp.size());
+    reduce_intervals(thrust::system::omp::tag(), input.begin(), output.begin(), thrust::plus<T>(), decomp);
+
+    ASSERT_EQUAL(output[0], 6);
+    ASSERT_EQUAL(output[1], 4);
+  }
+}
+DECLARE_UNITTEST(TestOmpReduceIntervalsSimple);
 
 
 template <typename T>
-struct TestReduceIntervals
+struct TestOmpReduceIntervals
 {
   void operator()(const size_t n)
   {
-    KNOWN_FAILURE;
+    using thrust::system::omp::detail::reduce_intervals;
+    using thrust::system::detail::internal::uniform_decomposition;
+    
+    thrust::host_vector<T>   h_input = unittest::random_integers<T>(n);
+    thrust::device_vector<T> d_input = h_input;
 
-//    namespace ns = thrust::system::omp::detail;
-//
-//    thrust::host_vector<T>   h_input = unittest::random_integers<T>(n);
-//    thrust::device_vector<T> d_input = h_input;
-//
-//    thrust::system::detail::internal::uniform_decomposition<size_t> decomp(n, 7, 100);
-//
-//    thrust::host_vector<T>   h_output(decomp.size());
-//    thrust::device_vector<T> d_output(decomp.size());
-//    
-//    ns::reduce_intervals(h_input.begin(), h_output.begin(), thrust::plus<T>(), decomp);
-//    ns::reduce_intervals(d_input.begin(), d_output.begin(), thrust::plus<T>(), decomp);
-//
-//    ASSERT_EQUAL(h_output, d_output);
+    uniform_decomposition<size_t> decomp(n, 7, 100);
+
+    thrust::host_vector<T>   h_output(decomp.size());
+    thrust::device_vector<T> d_output(decomp.size());
+    
+    ::reduce_intervals(h_input.begin(), h_output.begin(), thrust::plus<T>(), decomp);
+    reduce_intervals(thrust::system::omp::tag(), d_input.begin(), d_output.begin(), thrust::plus<T>(), decomp);
+
+    ASSERT_EQUAL(h_output, d_output);
   }
 };
-VariableUnitTest<TestReduceIntervals, IntegralTypes> TestReduceIntervalsInstance;
+VariableUnitTest<TestOmpReduceIntervals, IntegralTypes> TestOmpReduceIntervalsInstance;
 
