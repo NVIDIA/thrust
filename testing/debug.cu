bc5e5c906e49c47490d3f2f9cb22f3f726e7474a
Jared Hoberock <jaredhoberock@gmail.com>
2012-08-01 14:03:31 -0700

    Eliminate some KNOWN_FAILUREs from testing/debug.cu.

--- testing/debug.cu
+++ testing/debug.cu
@@ -12,7 +12,7 @@
 
 void TestTransformNullPtr(void)
 {
-#if defined(__APPLE__) || defined(_MSC_VER)
+#if defined(__APPLE__)
   KNOWN_FAILURE;
 #endif
 
@@ -90,25 +90,23 @@ DECLARE_UNITTEST(TestExclusiveScanNullPtr);
 
 void TestSortNullPtr(void)
 {
-  // XXX sort(null) below just crashes
-  KNOWN_FAILURE;
-//  thrust::device_ptr<int> ptr = thrust::device_pointer_cast<int>(0);
-//
-//  bool caught_exception = false;
-//
-//  try
-//  {
-//    thrust::sort(ptr,ptr+1);
-//  }
-//  catch(thrust::system_error e)
-//  {
-//    caught_exception = true;
-//
-//    // reset the cuda error
-//    cudaGetLastError();
-//  }
-//
-//  ASSERT_EQUAL(true, caught_exception);
+  thrust::device_ptr<int> ptr = thrust::device_pointer_cast<int>(0);
+
+  bool caught_exception = false;
+
+  try
+  {
+    thrust::sort(ptr,ptr+1);
+  }
+  catch(thrust::system_error e)
+  {
+    caught_exception = true;
+
+    // reset the cuda error
+    cudaGetLastError();
+  }
+
+  ASSERT_EQUAL(true, caught_exception);
 }
 DECLARE_UNITTEST(TestSortNullPtr);
 
