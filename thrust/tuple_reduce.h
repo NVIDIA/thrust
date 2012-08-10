337d252ff755922fc771a151428faf7b34ac590f
Bryan Catanzaro <bcatanzaro@acm.org>
2012-08-10 15:55:57 -0700

    Simplify.

--- thrust/tuple_reduce.h
+++ thrust/tuple_reduce.h
@@ -46,38 +46,6 @@ struct tuple_reduce_impl<F, thrust::detail::cons<HT, TT> > {
     }
 };
 
-
-template<typename F,
-         typename T0,
-         typename T1,
-         typename T2,
-         typename T3,
-         typename T4,
-         typename T5,
-         typename T6,
-         typename T7,
-         typename T8,
-         typename T9>
-struct tuple_reduce_impl<F,
-                         thrust::tuple<T0, T1, T2, T3, T4,
-                                       T5, T6, T7, T8, T9> > {
-    typedef thrust::tuple<T0, T1, T2, T3, T4,
-                          T5, T6, T7, T8, T9> input_tuple;
-    typedef typename F::result_type result_type;
-
-    __host__ __device__
-    static result_type fun(
-        const F& f,
-        const input_tuple& t,
-        const result_type& p) {
-        return tuple_reduce_impl<F,
-                                 thrust::detail::cons<
-                                     typename input_tuple::head_type,
-                                     typename input_tuple::tail_type> >
-            ::fun(f, t, p);
-    }
-};
-
 template<typename F>
 struct tuple_reduce_impl<F,
                          thrust::null_type> {
@@ -106,7 +74,11 @@ __host__ __device__
 typename F::result_type tuple_reduce(const F& f,
                                      const T& t,
                                      const typename F::result_type& p) {
-    return detail::tuple_reduce_impl<F, T>::fun(f, t, p);
+    return detail::tuple_reduce_impl<F,
+                                     thrust::detail::cons<
+                                         typename T::head_type,
+                                         typename T::tail_type>
+                                     >::fun(f, t, p);
 }
 
 /*! \p tuple_sum returns the arithmetic sum of all elements of a tuple.
