b05b1c43a28b485098cd3d41c25394052e0bfe0b
Jared Hoberock <jaredhoberock@gmail.com>
2012-09-24 16:44:51 -0700

    Check in currently not working merge kernel.

---
+++ merge.h
@@ -0,0 +1,138 @@
+#include <thrust/detail/config.h>
+#include <thrust/pair.h>
+#include <thrust/detail/minmax.h>
+#include <thrust/detail/function.h>
+#include <thrust/system/cuda/detail/detail/uninitialized.h>
+
+
+template<typename RandomAccessIterator1,
+         typename Size,
+         typename RandomAccessIterator2,
+         typename Compare>
+__device__ __thrust_forceinline__
+thrust::pair<Size,Size>
+  partition_search(RandomAccessIterator1 first1,
+                   RandomAccessIterator2 first2,
+                   Size diag,
+                   Size lower_bound1,
+                   Size upper_bound1,
+                   Size lower_bound2,
+                   Size upper_bound2,
+                   Compare comp)
+{
+  Size begin = thrust::max<Size>(lower_bound1, diag - upper_bound2);
+  Size end = thrust::min<Size>(diag - lower_bound2, upper_bound1);
+
+  while(begin < end)
+  {
+    Size mid = (begin + end) / 2;
+    Size index1 = mid;
+    Size index2 = diag - mid - 1;
+
+    if(comp(first2[index2] < first1[index1]))
+    {
+      end = mid;
+    }
+    else
+    {
+      begin = mid + 1;
+    }
+  }
+
+  return thrust::make_pair(begin, diag - begin)
+}
+
+
+template<unsigned int block_size, unsigned int per_thread, typename RandomAccessIterator1, typename Size, typename RandomAccessIterator2, typename RandomAccessIterator3, typename Compare>
+__global__
+void merge_n(RandomAccessIterator1 first1,
+             Size n1,
+             RandomAccessIterator2 first2,
+             Size n2,
+             RandomAccessIterator3 result,
+             Compare comp_)
+{
+  thrust::detail::device_function<Compare,bool> comp;
+
+  Size result_size = n1 + n2;
+  Size per_cta_size = (result_size-1)/gridDim.x+1;
+  Size cta_portion = thrust::min<Size>(per_cta_size, result_size-blockIdx.x*per_cta_size);
+  Size cta_offset = blockIdx.x*per_cta_size;
+
+  using thrust::system::cuda::detail::detail::uninitialized;
+  __shared__ uninitialized<thrust::pair<Size,Size> > block_begin;
+
+  if(threadIdx.x == 0)
+  {
+    block_begin = (blockIdx.x == 0) ?
+      thrust::make_pair(Size(0),Size(0)) : 
+      partition_search(first1, first2,
+                       per_cta_size * blockIdx.x,
+                       Size(0), n1,
+                       Size(0), n2,
+                       comp);
+  }
+
+  __syncthreads();
+
+  Size remaining_size = cta_portion;
+  Size processed_size = 0;
+  thrust::pair<Size,Size> offset = block_begin;
+
+  while(remaining_size > 0)
+  {
+    Size thread_begin1, thread_begin2;
+
+    thrust::pair<Size,Size> thread_begin =
+      partition_search(a, b, cta_offset+processed_size+threadIdx.x*per_thread,
+                       block_begin.first, thrust::min<Size>(offset.first + block_size*per_thread, n1),
+                       block_begin.second, thrust::min<Size>(offset.second + block_size*per_thread, n2),
+                       comp);
+
+    T x1 = first1[thread_begin.first];
+    T x2 = first2[thread_begin.second];
+
+    Size count = thrust::min<Size>(per_thread, result_size - thread_begin.first - thread_begin.second);
+    for(Size i = 0; i < count; ++i)
+    {
+       bool p = false;
+       if(thread_begin.second >= n2)
+       {
+         p = true;
+       }
+       else if(thread_begin.first >= n1)
+       {
+         p = false;
+       }
+       else
+       {
+         p = !comp(x2,x1);
+       }
+
+       result[cta_offset+processed_size+threadIdx.x*per_thread+i] = p ? x1 : x2;
+
+       if(p)
+       {
+         ++thread_begin.first;
+         x1 = first1[thread_begin.first];
+       }
+       else
+       {
+         ++thread_begin.second;
+         x2 = first2[thread_begin.second];
+       }
+    }
+
+    offset.first   += block_size*per_thread;
+    offset.second  += block_size*per_thread;
+    processed_size += block_size*per_thread;
+    remaining_size -= block_size*per_thread;
+
+    if(threadIdx.x == block_size-1)
+    {
+      block_begin = thread_begin;
+    }
+    __syncthreads();
+  } // end while
+}
+
