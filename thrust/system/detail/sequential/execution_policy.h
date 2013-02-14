aeb16f1c2cb548a17901eb6ff62bc0bb7402f349
Jared Hoberock <jaredhoberock@gmail.com>
2013-02-14 13:16:18 -0800

    Add malloc.h & free.h to sequential's execution_policy.h.

--- thrust/system/detail/sequential/execution_policy.h
+++ thrust/system/detail/sequential/execution_policy.h
@@ -46,6 +46,8 @@ static const tag seq;
 #include <thrust/system/detail/sequential/extrema.h>
 #include <thrust/system/detail/sequential/find.h>
 #include <thrust/system/detail/sequential/for_each.h>
+#include <thrust/system/detail/sequential/free.h>
+#include <thrust/system/detail/sequential/malloc.h>
 #include <thrust/system/detail/sequential/merge.h>
 #include <thrust/system/detail/sequential/reduce_by_key.h>
 #include <thrust/system/detail/sequential/reduce.h>
