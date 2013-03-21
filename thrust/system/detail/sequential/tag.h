651b78c6199a9a8cc2aa858a814c9343a5ac7cb4
Jared Hoberock <jaredhoberock@gmail.com>
2013-03-21 12:48:31 -0700

    Introduce thrust::system::detail::sequential::execution_policy

--- thrust/system/detail/sequential/tag.h
+++ thrust/system/detail/sequential/tag.h
@@ -28,10 +28,43 @@ namespace detail
 namespace sequential
 {
 
-struct tag : thrust::execution_policy<tag>
+
+// this awkward sequence of definitions arises
+// from the desire both for tag to derive
+// from execution_policy and for execution_policy
+// to convert to tag (when execution_policy is not
+// an ancestor of tag)
+
+// forward declaration of tag
+struct tag;
+
+// forward declaration of execution_policy
+template<typename> struct execution_policy;
+
+// specialize execution_policy for tag
+template<>
+  struct execution_policy<tag>
+    : thrust::execution_policy<tag>
+{};
+
+// tag's definition comes before the generic definition of execution_policy
+struct tag : execution_policy<tag>
 {
   __host__ __device__ tag() {}
 };
+
+// allow conversion to tag when it is not a successor
+template<typename Derived>
+  struct execution_policy
+    : thrust::execution_policy<Derived>
+{
+  // allow conversion to tag
+  inline operator tag () const
+  {
+    return tag();
+  }
+};
+
 
 } // end sequential
 } // end detail
