419f1aeda6d725a281f5d1a0c3a35b1b5db9fad4
Jared Hoberock <jaredhoberock@gmail.com>
2013-03-25 16:39:36 -0700

    Avoid #including <thrust/extrema.h> in the generic implementation of find. Use thrust::min instead of std::min in the generic implementation of find. Annotate execution_policy-based find algorithms with __host__ __device__.

--- thrust/system/detail/generic/find.h
+++ thrust/system/detail/generic/find.h
@@ -29,23 +29,30 @@ namespace detail
 namespace generic
 {
 
+
 template<typename DerivedPolicy, typename InputIterator, typename T>
+__host__ __device__
 InputIterator find(thrust::execution_policy<DerivedPolicy> &exec,
                    InputIterator first,
                    InputIterator last,
                    const T& value);
 
+
 template<typename DerivedPolicy, typename InputIterator, typename Predicate>
+__host__ __device__
 InputIterator find_if(thrust::execution_policy<DerivedPolicy> &exec,
                       InputIterator first,
                       InputIterator last,
                       Predicate pred);
 
+
 template<typename DerivedPolicy, typename InputIterator, typename Predicate>
+__host__ __device__
 InputIterator find_if_not(thrust::execution_policy<DerivedPolicy> &exec,
                           InputIterator first,
                           InputIterator last,
                           Predicate pred);
+
 
 } // end namespace generic
 } // end namespace detail
