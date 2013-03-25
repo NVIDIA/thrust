b04343b45215a9edb5e14e6eb9a17e722e621f40
Jared Hoberock <jaredhoberock@gmail.com>
2013-03-25 14:39:09 -0700

    Annotate execution_policy based merge functions with __host__ __device__.

--- thrust/system/detail/generic/merge.h
+++ thrust/system/detail/generic/merge.h
@@ -36,6 +36,7 @@ template<typename DerivedPolicy,
          typename InputIterator2,
          typename OutputIterator,
          typename StrictWeakOrdering>
+__host__ __device__
   OutputIterator merge(thrust::execution_policy<DerivedPolicy> &exec,
                        InputIterator1 first1,
                        InputIterator1 last1,
@@ -49,6 +50,7 @@ template<typename DerivedPolicy,
          typename InputIterator1,
          typename InputIterator2,
          typename OutputIterator>
+__host__ __device__
   OutputIterator merge(thrust::execution_policy<DerivedPolicy> &exec,
                        InputIterator1 first1,
                        InputIterator1 last1,
@@ -58,6 +60,7 @@ template<typename DerivedPolicy,
 
 
 template<typename DerivedPolicy, typename InputIterator1, typename InputIterator2, typename InputIterator3, typename InputIterator4, typename OutputIterator1, typename OutputIterator2, typename Compare>
+__host__ __device__
   thrust::pair<OutputIterator1,OutputIterator2>
     merge_by_key(thrust::execution_policy<DerivedPolicy> &exec,
                  InputIterator1 keys_first1, InputIterator1 keys_last1,
@@ -69,6 +72,7 @@ template<typename DerivedPolicy, typename InputIterator1, typename InputIterator
 
 
 template<typename DerivedPolicy, typename InputIterator1, typename InputIterator2, typename InputIterator3, typename InputIterator4, typename OutputIterator1, typename OutputIterator2>
+__host__ __device__
   thrust::pair<OutputIterator1,OutputIterator2>
     merge_by_key(thrust::execution_policy<DerivedPolicy> &exec,
                  InputIterator1 keys_first1, InputIterator1 keys_last1,
