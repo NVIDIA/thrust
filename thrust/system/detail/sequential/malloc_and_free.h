754ec8c0348867a4b8876c275dbd50cc62b5ed22
Jared Hoberock <jaredhoberock@gmail.com>
2013-03-28 17:07:28 -0700

    Move the __CUDA_ARCH__ guard for sequential::malloc & sequential::free back inside the functions.

--- thrust/system/detail/sequential/malloc_and_free.h
+++ thrust/system/detail/sequential/malloc_and_free.h
@@ -31,24 +31,26 @@ namespace sequential
 {
 
 
-#if !defined(__CUDA_ARCH__) || (__CUDA_ARCH__ > 200)
 template<typename DerivedPolicy>
 inline __host__ __device__
 void *malloc(execution_policy<DerivedPolicy> &, std::size_t n)
 {
-  return std::malloc(n);
-} // end mallc()
-#endif
-
-
 #if !defined(__CUDA_ARCH__) || (__CUDA_ARCH__ > 200)
+  return std::malloc(n);
+#else
+  return 0;
+#endif
+} // end mallc()
+
+
 template<typename DerivedPolicy, typename Pointer>
 inline __host__ __device__
 void free(sequential::execution_policy<DerivedPolicy> &, Pointer ptr)
 {
+#if !defined(__CUDA_ARCH__) || (__CUDA_ARCH__ > 200)
   std::free(thrust::raw_pointer_cast(ptr));
-} // end mallc()
 #endif
+} // end mallc()
 
 
 } // end sequential
