458f1d82a8738c877c80c011644bbbeaa200b750
Jared Hoberock <jaredhoberock@gmail.com>
2013-04-02 14:50:49 -0700

    Test for_each & for_each_n with thrust::seq in __device__ code.

--- testing/backend/cuda/for_each.cu
+++ testing/backend/cuda/for_each.cu
@@ -1,5 +1,6 @@
 #include <unittest/unittest.h>
 #include <thrust/for_each.h>
+#include <thrust/execution_policy.h>
 
 static const size_t NUM_REGISTERS = 100;
 
@@ -46,4 +47,86 @@ void TestForEachNLargeRegisterFootprint()
   thrust::for_each_n(input.begin(), input.size(), CopyFunctorWithManyRegisters<NUM_REGISTERS>());
 }
 DECLARE_UNITTEST(TestForEachNLargeRegisterFootprint);
+
+
+template <typename T>
+struct mark_present_for_each
+{
+  T * ptr;
+  __host__ __device__ void
+  operator()(T x){ ptr[(int) x] = 1; }
+};
+
+
+template<typename Iterator, typename Function>
+__global__ void test_for_each_device_seq_kernel(Iterator first, Iterator last, Function f)
+{
+  thrust::for_each(thrust::seq, first, last, f);
+}
+
+
+template<typename T>
+void TestForEachDeviceSeq(const size_t n)
+{
+  const size_t output_size = std::min((size_t) 10, 2 * n);
+  
+  thrust::host_vector<T> h_input = unittest::random_integers<T>(n);
+  
+  for(size_t i = 0; i < n; i++)
+    h_input[i] =  ((size_t) h_input[i]) % output_size;
+  
+  thrust::device_vector<T> d_input = h_input;
+  
+  thrust::host_vector<T>   h_output(output_size, (T) 0);
+  thrust::device_vector<T> d_output(output_size, (T) 0);
+  
+  mark_present_for_each<T> h_f;
+  mark_present_for_each<T> d_f;
+  h_f.ptr = &h_output[0];
+  d_f.ptr = (&d_output[0]).get();
+  
+  thrust::for_each(h_input.begin(), h_input.end(), h_f);
+  
+  test_for_each_device_seq_kernel<<<1,1>>>(d_input.begin(), d_input.end(), d_f);
+  
+  ASSERT_EQUAL(h_output, d_output);
+}
+DECLARE_VARIABLE_UNITTEST(TestForEachDeviceSeq);
+
+
+template<typename Iterator, typename Size, typename Function>
+__global__
+void test_for_each_n_device_seq_kernel(Iterator first, Size n, Function f)
+{
+  thrust::for_each_n(thrust::seq, first, n, f);
+}
+
+
+template<typename T>
+void TestForEachNDeviceSeq(const size_t n)
+{
+  const size_t output_size = std::min((size_t) 10, 2 * n);
+  
+  thrust::host_vector<T> h_input = unittest::random_integers<T>(n);
+  
+  for(size_t i = 0; i < n; i++)
+    h_input[i] =  ((size_t) h_input[i]) % output_size;
+  
+  thrust::device_vector<T> d_input = h_input;
+  
+  thrust::host_vector<T>   h_output(output_size, (T) 0);
+  thrust::device_vector<T> d_output(output_size, (T) 0);
+  
+  mark_present_for_each<T> h_f;
+  mark_present_for_each<T> d_f;
+  h_f.ptr = &h_output[0];
+  d_f.ptr = (&d_output[0]).get();
+  
+  thrust::for_each_n(h_input.begin(), h_input.size(), h_f);
+  
+  test_for_each_n_device_seq_kernel<<<1,1>>>(d_input.begin(), d_input.size(), d_f);
+  
+  ASSERT_EQUAL(h_output, d_output);
+}
+DECLARE_VARIABLE_UNITTEST(TestForEachNDeviceSeq);
 
