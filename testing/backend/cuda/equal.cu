6d85d360939e6853a2332c0152c667300472807b
Jared Hoberock <jaredhoberock@gmail.com>
2013-04-02 15:25:31 -0700

    Test equal with thrust::seq in __device__ code.

---
+++ testing/backend/cuda/equal.cu
@@ -0,0 +1,54 @@
+#include <unittest/unittest.h>
+#include <thrust/equal.h>
+#include <thrust/functional.h>
+#include <thrust/execution_policy.h>
+
+
+template<typename Iterator1, typename Iterator2, typename Iterator3>
+__global__
+void equal_kernel(Iterator1 first1, Iterator1 last1, Iterator2 first2, Iterator3 result)
+{
+  *result = thrust::equal(thrust::seq, first1, last1, first2);
+}
+
+
+template<typename Iterator1, typename Iterator2, typename BinaryPredicate, typename Iterator3>
+__global__
+void equal_kernel(Iterator1 first1, Iterator1 last1, Iterator2 first2, BinaryPredicate pred, Iterator3 result)
+{
+  *result = thrust::equal(thrust::seq, first1, last1, first2, pred);
+}
+
+
+template<typename T>
+void TestEqualDeviceSeq(const size_t n)
+{
+  thrust::device_vector<T> d_data1 = unittest::random_samples<T>(n);
+  thrust::device_vector<T> d_data2 = unittest::random_samples<T>(n);
+  thrust::device_vector<bool> d_result(1, false);
+  
+  //empty ranges
+  equal_kernel<<<1,1>>>(d_data1.begin(), d_data1.begin(), d_data1.begin(), d_result.begin());
+  ASSERT_EQUAL(d_result[0], true);
+  
+  //symmetric cases
+  equal_kernel<<<1,1>>>(d_data1.begin(), d_data1.end(), d_data1.begin(), d_result.begin());
+  ASSERT_EQUAL(d_result[0], true);
+  
+  if(n > 0)
+  {
+    d_data1[0] = 0; d_data2[0] = 1;
+    
+    //different vectors
+    equal_kernel<<<1,1>>>(d_data1.begin(), d_data1.end(), d_data2.begin(), d_result.begin());
+    ASSERT_EQUAL(d_result[0], false);
+    
+    //different predicates
+    equal_kernel<<<1,1>>>(d_data1.begin(), d_data1.begin() + 1, d_data2.begin(), thrust::less<T>(), d_result.begin());
+    ASSERT_EQUAL(d_result[0], true);
+    equal_kernel<<<1,1>>>(d_data1.begin(), d_data1.begin() + 1, d_data2.begin(), thrust::greater<T>(), d_result.begin());
+    ASSERT_EQUAL(d_result[0], false);
+  }
+}
+DECLARE_VARIABLE_UNITTEST(TestEqualDeviceSeq);
+
