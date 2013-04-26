466f72b6d8fc83af8c549cfa87d482b0785858f2
Jared Hoberock <jaredhoberock@gmail.com>
2013-04-26 15:12:55 -0700

    Test logical algorithms with thrust::device in __device__ code.

--- testing/backend/cuda/logical.cu
+++ testing/backend/cuda/logical.cu
@@ -4,115 +4,157 @@
 #include <thrust/execution_policy.h>
 
 
-template<typename Iterator, typename Function, typename Iterator2>
+template<typename ExecutionPolicy, typename Iterator, typename Function, typename Iterator2>
 __global__
-void all_of_kernel(Iterator first, Iterator last, Function f, Iterator2 result)
+void all_of_kernel(ExecutionPolicy exec, Iterator first, Iterator last, Function f, Iterator2 result)
 {
-  *result = thrust::all_of(thrust::seq, first, last, f);
+  *result = thrust::all_of(exec, first, last, f);
+}
+
+
+template<typename ExecutionPolicy>
+void TestAllOfDevice(ExecutionPolicy exec)
+{
+  typedef int T;
+  thrust::device_vector<T> v(3, 1);
+  thrust::device_vector<bool> result(1);
+  
+  all_of_kernel<<<1,1>>>(exec, v.begin(), v.end(), thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(true, result[0]);
+  
+  v[1] = 0;
+  
+  all_of_kernel<<<1,1>>>(exec, v.begin(), v.end(), thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(false, result[0]);
+  
+  all_of_kernel<<<1,1>>>(exec, v.begin() + 0, v.begin() + 0, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(true, result[0]);
+
+  all_of_kernel<<<1,1>>>(exec, v.begin() + 0, v.begin() + 1, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(true, result[0]);
+
+  all_of_kernel<<<1,1>>>(exec, v.begin() + 0, v.begin() + 2, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(false, result[0]);
+
+  all_of_kernel<<<1,1>>>(exec, v.begin() + 1, v.begin() + 2, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(false, result[0]);
 }
 
 
 void TestAllOfDeviceSeq()
 {
-  typedef int T;
-  thrust::device_vector<T> v(3, 1);
-  thrust::device_vector<bool> result(1);
-  
-  all_of_kernel<<<1,1>>>(v.begin(), v.end(), thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(true, result[0]);
-  
-  v[1] = 0;
-  
-  all_of_kernel<<<1,1>>>(v.begin(), v.end(), thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(false, result[0]);
-  
-  all_of_kernel<<<1,1>>>(v.begin() + 0, v.begin() + 0, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(true, result[0]);
-
-  all_of_kernel<<<1,1>>>(v.begin() + 0, v.begin() + 1, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(true, result[0]);
-
-  all_of_kernel<<<1,1>>>(v.begin() + 0, v.begin() + 2, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(false, result[0]);
-
-  all_of_kernel<<<1,1>>>(v.begin() + 1, v.begin() + 2, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(false, result[0]);
+  TestAllOfDevice(thrust::seq);
 }
 DECLARE_UNITTEST(TestAllOfDeviceSeq);
 
 
-template<typename Iterator, typename Function, typename Iterator2>
-__global__
-void any_of_kernel(Iterator first, Iterator last, Function f, Iterator2 result)
+void TestAllOfDeviceDevice()
 {
-  *result = thrust::any_of(thrust::seq, first, last, f);
+  TestAllOfDevice(thrust::device);
+}
+DECLARE_UNITTEST(TestAllOfDeviceDevice);
+
+
+template<typename ExecutionPolicy, typename Iterator, typename Function, typename Iterator2>
+__global__
+void any_of_kernel(ExecutionPolicy exec, Iterator first, Iterator last, Function f, Iterator2 result)
+{
+  *result = thrust::any_of(exec, first, last, f);
+}
+
+
+template<typename ExecutionPolicy>
+void TestAnyOfDevice(ExecutionPolicy exec)
+{
+  typedef int T;
+  
+  thrust::device_vector<T> v(3, 1);
+  thrust::device_vector<bool> result(1);
+  
+  any_of_kernel<<<1,1>>>(exec, v.begin(), v.end(), thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(true, result[0]);
+  
+  v[1] = 0;
+  
+  any_of_kernel<<<1,1>>>(exec, v.begin(), v.end(), thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(true, result[0]);
+  
+  any_of_kernel<<<1,1>>>(exec, v.begin() + 0, v.begin() + 0, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(false, result[0]);
+
+  any_of_kernel<<<1,1>>>(exec, v.begin() + 0, v.begin() + 1, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(true, result[0]);
+
+  any_of_kernel<<<1,1>>>(exec, v.begin() + 0, v.begin() + 2, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(true, result[0]);
+
+  any_of_kernel<<<1,1>>>(exec, v.begin() + 1, v.begin() + 2, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(false, result[0]);
 }
 
 
 void TestAnyOfDeviceSeq()
 {
+  TestAnyOfDevice(thrust::seq);
+}
+DECLARE_UNITTEST(TestAnyOfDeviceSeq);
+
+
+void TestAnyOfDeviceDevice()
+{
+  TestAnyOfDevice(thrust::device);
+}
+DECLARE_UNITTEST(TestAnyOfDeviceDevice);
+
+
+template<typename ExecutionPolicy, typename Iterator, typename Function, typename Iterator2>
+__global__
+void none_of_kernel(ExecutionPolicy exec, Iterator first, Iterator last, Function f, Iterator2 result)
+{
+  *result = thrust::none_of(exec, first, last, f);
+}
+
+
+template<typename ExecutionPolicy>
+void TestNoneOfDevice(ExecutionPolicy exec)
+{
   typedef int T;
   
   thrust::device_vector<T> v(3, 1);
   thrust::device_vector<bool> result(1);
   
-  any_of_kernel<<<1,1>>>(v.begin(), v.end(), thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(true, result[0]);
+  none_of_kernel<<<1,1>>>(exec, v.begin(), v.end(), thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(false, result[0]);
   
   v[1] = 0;
   
-  any_of_kernel<<<1,1>>>(v.begin(), v.end(), thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(true, result[0]);
+  none_of_kernel<<<1,1>>>(exec, v.begin(), v.end(), thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(false, result[0]);
   
-  any_of_kernel<<<1,1>>>(v.begin() + 0, v.begin() + 0, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(false, result[0]);
-
-  any_of_kernel<<<1,1>>>(v.begin() + 0, v.begin() + 1, thrust::identity<T>(), result.begin());
+  none_of_kernel<<<1,1>>>(exec, v.begin() + 0, v.begin() + 0, thrust::identity<T>(), result.begin());
   ASSERT_EQUAL(true, result[0]);
 
-  any_of_kernel<<<1,1>>>(v.begin() + 0, v.begin() + 2, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(true, result[0]);
-
-  any_of_kernel<<<1,1>>>(v.begin() + 1, v.begin() + 2, thrust::identity<T>(), result.begin());
+  none_of_kernel<<<1,1>>>(exec, v.begin() + 0, v.begin() + 1, thrust::identity<T>(), result.begin());
   ASSERT_EQUAL(false, result[0]);
-}
-DECLARE_UNITTEST(TestAnyOfDeviceSeq);
 
+  none_of_kernel<<<1,1>>>(exec, v.begin() + 0, v.begin() + 2, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(false, result[0]);
 
-template<typename Iterator, typename Function, typename Iterator2>
-__global__
-void none_of_kernel(Iterator first, Iterator last, Function f, Iterator2 result)
-{
-  *result = thrust::none_of(thrust::seq, first, last, f);
+  none_of_kernel<<<1,1>>>(exec, v.begin() + 1, v.begin() + 2, thrust::identity<T>(), result.begin());
+  ASSERT_EQUAL(true, result[0]);
 }
 
 
 void TestNoneOfDeviceSeq()
 {
-  typedef int T;
-  
-  thrust::device_vector<T> v(3, 1);
-  thrust::device_vector<bool> result(1);
-  
-  none_of_kernel<<<1,1>>>(v.begin(), v.end(), thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(false, result[0]);
-  
-  v[1] = 0;
-  
-  none_of_kernel<<<1,1>>>(v.begin(), v.end(), thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(false, result[0]);
-  
-  none_of_kernel<<<1,1>>>(v.begin() + 0, v.begin() + 0, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(true, result[0]);
-
-  none_of_kernel<<<1,1>>>(v.begin() + 0, v.begin() + 1, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(false, result[0]);
-
-  none_of_kernel<<<1,1>>>(v.begin() + 0, v.begin() + 2, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(false, result[0]);
-
-  none_of_kernel<<<1,1>>>(v.begin() + 1, v.begin() + 2, thrust::identity<T>(), result.begin());
-  ASSERT_EQUAL(true, result[0]);
+  TestNoneOfDevice(thrust::seq);
 }
 DECLARE_UNITTEST(TestNoneOfDeviceSeq);
+
+
+void TestNoneOfDeviceDevice()
+{
+  TestNoneOfDevice(thrust::device);
+}
+DECLARE_UNITTEST(TestNoneOfDeviceDevice);
 
