0901510434232ff8191a8e0f94885e09e29f3bdd
Jared Hoberock <jaredhoberock@gmail.com>
2013-05-18 16:30:25 -0700

    Test unaligned radix sorts on the device.

--- testing/backend/cuda/radix_sort_by_key_unaligned.cu
+++ testing/backend/cuda/radix_sort_by_key_unaligned.cu
@@ -2,13 +2,8 @@
 #include <thrust/functional.h>
 #include <thrust/sequence.h>
 #include <thrust/device_malloc_allocator.h>
-
 #include <thrust/sort.h>
 #include <thrust/system/cuda/detail/detail/stable_radix_sort.h>
-
-#if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
-
-using namespace unittest;
 
 typedef unittest::type_list<
 #if !(defined(__GNUC__) && (__GNUC__ <= 4) && (__GNUC_MINOR__ <= 1))
@@ -20,7 +15,7 @@ typedef unittest::type_list<
                             unsigned long,
                             unsigned long long> UnsignedIntegerTypes;
 
-template <typename T>
+template<typename T>
 struct TestRadixSortByKeyUnaligned
 {
   void operator()(const size_t n)
@@ -58,5 +53,58 @@ struct TestRadixSortByKeyUnaligned
 };
 VariableUnitTest<TestRadixSortByKeyUnaligned, UnsignedIntegerTypes> TestRadixSortByKeyUnalignedInstance;
 
-#endif // THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
+
+template<typename ExecutionPolicy, typename Iterator1, typename Iterator2, typename Iterator3>
+__global__ void stable_radix_sort_by_key_kernel(ExecutionPolicy exec, Iterator1 keys_first, Iterator1 keys_last, Iterator2 values_first, Iterator3 is_supported)
+{
+#if __CUDA_ARCH__ < 350
+  *is_supported = false;
+#else
+  *is_supported = true;
+  thrust::system::cuda::detail::detail::stable_radix_sort_by_key(exec, keys_first, keys_last, values_first);
+#endif
+}
+
+
+template<typename T>
+struct TestRadixSortByKeyUnalignedDeviceDevice
+{
+  void operator()(const size_t n)
+  {
+    typedef thrust::device_vector<T>   Vector1;
+    typedef thrust::device_vector<int> Vector2;
+
+    Vector1 unsorted_keys = unittest::random_integers<T>(n);
+    Vector1   sorted_keys = unsorted_keys;
+
+    Vector2 unsorted_values(n); thrust::sequence(unsorted_values.begin(), unsorted_values.end());
+    Vector2   sorted_values = unsorted_values;
+    
+    thrust::sort_by_key(sorted_keys.begin(), sorted_keys.end(), sorted_values.begin());
+
+    for(int offset = 1; offset < 4; offset++)
+    {
+        Vector1   unaligned_unsorted_keys(n + offset, 0);
+        Vector1     unaligned_sorted_keys(n + offset, 0);
+        Vector2 unaligned_unsorted_values(n + offset, 0);
+        Vector2   unaligned_sorted_values(n + offset, 0);
+        
+        thrust::copy(  unsorted_keys.begin(),   unsorted_keys.end(),   unaligned_unsorted_keys.begin() + offset);
+        thrust::copy(    sorted_keys.begin(),     sorted_keys.end(),     unaligned_sorted_keys.begin() + offset);
+        thrust::copy(unsorted_values.begin(), unsorted_values.end(), unaligned_unsorted_values.begin() + offset);
+        thrust::copy(  sorted_values.begin(),   sorted_values.end(),   unaligned_sorted_values.begin() + offset);
+
+        thrust::device_vector<bool> is_supported(1);
+   
+        stable_radix_sort_by_key_kernel<<<1,1>>>(thrust::device, unaligned_unsorted_keys.begin() + offset, unaligned_unsorted_keys.end(), unaligned_unsorted_values.begin() + offset, is_supported.begin());
+
+        if(is_supported[0])
+        {
+          ASSERT_EQUAL(  unaligned_unsorted_keys,   unaligned_sorted_keys);
+          ASSERT_EQUAL(unaligned_unsorted_values, unaligned_sorted_values);
+        }
+    }
+  }
+};
+VariableUnitTest<TestRadixSortByKeyUnalignedDeviceDevice, UnsignedIntegerTypes> TestRadixSortByKeyUnalignedDeviceDeviceInstance;
 
