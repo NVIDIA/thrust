339ef5acd045cb9e79e109a0f3c8f0ee2ae2c534
Jared Hoberock <jaredhoberock@gmail.com>
2013-05-18 16:30:25 -0700

    Test unaligned radix sorts on the device.

--- testing/backend/cuda/radix_sort_unaligned.cu
+++ testing/backend/cuda/radix_sort_unaligned.cu
@@ -2,11 +2,8 @@
 #include <thrust/functional.h>
 #include <thrust/sequence.h>
 #include <thrust/device_malloc_allocator.h>
-
 #include <thrust/sort.h>
 #include <thrust/system/cuda/detail/detail/stable_radix_sort.h>
-
-#if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
 
 typedef unittest::type_list<
 #if !(defined(__GNUC__) && (__GNUC__ <= 4) && (__GNUC_MINOR__ <= 1))
@@ -18,7 +15,7 @@ typedef unittest::type_list<
                             unsigned long,
                             unsigned long long> UnsignedIntegerTypes;
 
-template <typename T>
+template<typename T>
 struct TestRadixSortUnaligned
 {
   void operator()(const size_t n)
@@ -47,5 +44,49 @@ struct TestRadixSortUnaligned
 };
 VariableUnitTest<TestRadixSortUnaligned, UnsignedIntegerTypes> TestRadixSortUnalignedInstance;
 
-#endif // THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
+
+template<typename ExecutionPolicy, typename Iterator1, typename Iterator2>
+__global__ void stable_radix_sort_kernel(ExecutionPolicy exec, Iterator1 first, Iterator1 last, Iterator2 is_supported)
+{
+#if __CUDA_ARCH__ < 350
+  *is_supported = false;
+#else
+  *is_supported = true;
+  thrust::system::cuda::detail::detail::stable_radix_sort(exec, first, last);
+#endif
+}
+
+
+template<typename T>
+struct TestRadixSortUnalignedDeviceDevice
+{
+  void operator()(const size_t n)
+  {
+    typedef thrust::device_vector<T> Vector;
+
+    Vector unsorted_keys = unittest::random_integers<T>(n);
+    Vector   sorted_keys = unsorted_keys;
+    
+    thrust::sort(sorted_keys.begin(), sorted_keys.end());
+
+    for(int offset = 1; offset < 4; offset++)
+    {
+        Vector unaligned_unsorted_keys(n + offset, 0);
+        Vector   unaligned_sorted_keys(n + offset, 0);
+        
+        thrust::copy(unsorted_keys.begin(), unsorted_keys.end(), unaligned_unsorted_keys.begin() + offset);
+        thrust::copy(  sorted_keys.begin(),   sorted_keys.end(),   unaligned_sorted_keys.begin() + offset);
+
+        thrust::device_vector<bool> is_supported(1);
+   
+        stable_radix_sort_kernel<<<1,1>>>(thrust::device, unaligned_unsorted_keys.begin() + offset, unaligned_unsorted_keys.end(), is_supported.begin());
+
+        if(is_supported[0])
+        {
+          ASSERT_EQUAL(unaligned_unsorted_keys, unaligned_sorted_keys);
+        }
+    }
+  }
+};
+VariableUnitTest<TestRadixSortUnalignedDeviceDevice, UnsignedIntegerTypes> TestRadixSortUnalignedDeviceDeviceInstance;
 
