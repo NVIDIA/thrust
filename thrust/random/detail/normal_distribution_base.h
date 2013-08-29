dfc1adec0128ed157f7381cd66f21d41f48c56df
Jared Hoberock <jaredhoberock@gmail.com>
2013-08-29 11:56:05 -0700

    Eliminate some warnings in normal_distribution_base.

--- thrust/random/detail/normal_distribution_base.h
+++ thrust/random/detail/normal_distribution_base.h
@@ -84,11 +84,11 @@ template<typename RealType>
 {
   protected:
     normal_distribution_portable()
-      : m_valid(false)
+      : m_r1(), m_r2(), m_cached_rho(), m_valid(false)
     {}
 
     normal_distribution_portable(const normal_distribution_portable &other)
-      : m_valid(other.m_valid)
+      : m_r1(other.m_r1), m_r2(other.m_r2), m_cached_rho(other.m_cached_rho), m_valid(other.m_valid)
     {}
 
     void reset()
