f7cab3ce1b1d59258271d2d854ed21e9858048f6
Filipe Maia <filipe.maia@icm.uu.se>
2013-12-06 17:19:33 +0100

    Exaustive testing of complex transforms

--- testing/complex_transform.cu
+++ testing/complex_transform.cu
@@ -2,6 +2,7 @@
 #include <thrust/host_vector.h>
 #include <thrust/complex.h>
 #include <thrust/transform.h>
+#include <iostream>
 
 struct basic_arithmetic_functor
 {
@@ -16,19 +17,18 @@ struct basic_arithmetic_functor
   } // end operator()()
 }; // end make_pair_functor
 
-struct general_functor
+struct complex_plane_functor
 {
   template<typename T>
   __host__ __device__
   thrust::complex<T> operator()(const thrust::complex<T> &x)
   {
-    // exercise general functions
-    // Should return approximately 1
+    // Should return a proximately 1
     return thrust::proj( (thrust::polar(abs(x),arg(x)) * conj(x))/norm(x));
   } // end operator()()
 }; // end make_pair_functor
 
-struct power_functor
+struct pow_functor
 {
   template<typename T>
   __host__ __device__
@@ -36,101 +36,348 @@ struct power_functor
 				const thrust::complex<T> &y)
   {
     // exercise power functions
-    return pow(x,y)+sqrt(x);
+    return pow(x,y);
   } // end operator()()
 }; // end make_pair_functor
 
-struct exponential_functor
+struct sqrt_functor
 {
   template<typename T>
   __host__ __device__
   thrust::complex<T> operator()(const thrust::complex<T> &x)
   {
     // exercise power functions
-    // should return approximately 1
-    return log(exp(x))/(T(2.30258509299404568402)*log10(exp(x)));
+    return sqrt(x);
   } // end operator()()
 }; // end make_pair_functor
 
-struct trigonometric_functor
+struct log_functor
 {
   template<typename T>
   __host__ __device__
   thrust::complex<T> operator()(const thrust::complex<T> &x)
   {
-    // exercise power functions
-    // might return approximately 1
-    return acos(cos(x))+asin(sin(x))-T(4.0)*x
-      +(acosh(cosh(x)) + asinh(sinh(x)));// + atanh(tanh(x)));
-//+atan(tan(x));
-      //      (acosh(cosh(x)) + asinh(sinh(x)) + atanh(tanh(x)));
+    return log(x);
   } // end operator()()
 }; // end make_pair_functor
+
+struct exp_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return exp(x);
+  } // end operator()()
+}; // end make_pair_functor
+
+struct log10_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return log10(x);
+  } // end operator()()
+}; // end make_pair_functor
+
+
+struct cos_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return cos(x);
+  } 
+}; 
+
+struct sin_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return sin(x);
+  } 
+}; 
+
+struct tan_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return tan(x);
+  } 
+}; 
+
+
+
+struct cosh_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return cosh(x);
+  } 
+}; 
+
+struct sinh_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return sinh(x);
+  } 
+}; 
+
+struct tanh_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return tanh(x);
+  } 
+}; 
+
+
+struct acos_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return acos(x);
+  } 
+}; 
+
+struct asin_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return asin(x);
+  } 
+}; 
+
+struct atan_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return atan(x);
+  } 
+}; 
+
+
+struct acosh_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return acosh(x);
+  } 
+}; 
+
+struct asinh_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return asinh(x);
+  } 
+}; 
+
+struct atanh_functor
+{
+  template<typename T>
+  __host__ __device__
+  thrust::complex<T> operator()(const thrust::complex<T> &x)
+  {
+    return atanh(x);
+  } 
+}; 
 
 
 template <typename T>
-struct TestComplexTransform
+thrust::host_vector<thrust::complex<T> > random_complex_samples(size_t n){
+  thrust::host_vector<T> real = unittest::random_samples<T>(2*n);
+  thrust::host_vector<thrust::complex<T> > h_p1(n);
+  for(size_t i = 0; i<n; i++){
+    h_p1[i].real(real[i]);
+    h_p1[i].imag(real[2*i]);
+  }
+  return h_p1;
+}
+
+template <typename T>
+struct TestComplexArithmeticTransform
 {
   void operator()(const size_t n)
   {
     typedef thrust::complex<T> type;
-
-    thrust::host_vector<T> real = unittest::random_samples<T>(n);
-    thrust::host_vector<T> imag = unittest::random_samples<T>(n);
-    thrust::host_vector<type> h_p1(n);
-    thrust::host_vector<type> h_p2(n);
-
-    for(size_t i = 0; i<n; i++){
-      h_p1[i].real(real[i]);
-      h_p1[i].imag(imag[i]);
-    }
-
-    real = unittest::random_samples<T>(n);
-    imag = unittest::random_samples<T>(n);
-    for(size_t i = 0; i<n; i++){
-      h_p2[i].real(real[i]);
-      h_p2[i].imag(imag[i]);
-    }
+    thrust::host_vector<type> h_p1 = random_complex_samples<T>(n);
+    thrust::host_vector<type> h_p2 = random_complex_samples<T>(n);
     thrust::host_vector<type>   h_result(n);
 
     thrust::device_vector<type> d_p1 = h_p1;
     thrust::device_vector<type> d_p2 = h_p2;
     thrust::device_vector<type> d_result(n);
 
-    // run basic arithmetic on the host
     thrust::transform(h_p1.begin(), h_p1.end(), h_p2.begin(), h_result.begin(), basic_arithmetic_functor());
-    // run basic arithmetic on the host
     thrust::transform(d_p1.begin(), d_p1.end(), d_p2.begin(), d_result.begin(), basic_arithmetic_functor());    
-    // Currently just checking for compilation
-    ASSERT_ALMOST_EQUAL(h_result, d_result);
-    
-    // run general functions on the host
-    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), general_functor());
-    // run general functions on the host
-    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), general_functor());
-    // Currently just checking for compilation
-    ASSERT_ALMOST_EQUAL(h_result, d_result);
-
-    // run power functions on the host
-    thrust::transform(h_p1.begin(), h_p1.end(), h_p2.begin(), h_result.begin(), power_functor());
-    // run power functions on the host
-    thrust::transform(d_p1.begin(), d_p1.end(), d_p2.begin(), d_result.begin(), power_functor());
-    // Currently just checking for compilation
-    ASSERT_ALMOST_EQUAL(h_result, d_result);
-
-    // run exponential functions on the host
-    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), exponential_functor());
-    // run exponential functions on the host
-    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), exponential_functor());
-    // Currently just checking for compilation
-    ASSERT_ALMOST_EQUAL(h_result, d_result);
-
-    // run trigonometric functions on the host
-    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), trigonometric_functor());
-    // run trigonometric functions on the host
-    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), trigonometric_functor());
-    // Currently just checking for compilation
     ASSERT_ALMOST_EQUAL(h_result, d_result);
   }
 };
-VariableUnitTest<TestComplexTransform, FloatingPointTypes> TestComplexTransformInstance;
+VariableUnitTest<TestComplexArithmeticTransform, FloatingPointTypes> TestComplexArithmeticTransformInstance;
+
+template <typename T>
+struct TestComplexPlaneTransform
+{
+  void operator()(const size_t n)
+  {
+    typedef thrust::complex<T> type;
+    thrust::host_vector<type> h_p1 = random_complex_samples<T>(n);
+    thrust::host_vector<type>   h_result(n);
+
+    thrust::device_vector<type> d_p1 = h_p1;
+    thrust::device_vector<type> d_result(n);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), complex_plane_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), complex_plane_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+  }
+};
+VariableUnitTest<TestComplexPlaneTransform, FloatingPointTypes> TestComplexPlaneTransformInstance;
+
+
+template <typename T>
+struct TestComplexPowerTransform
+{
+  void operator()(const size_t n)
+  {
+    typedef thrust::complex<T> type;
+    thrust::host_vector<type> h_p1 = random_complex_samples<T>(n);
+    thrust::host_vector<type> h_p2 = random_complex_samples<T>(n);
+    thrust::host_vector<type>   h_result(n);
+
+    thrust::device_vector<type> d_p1 = h_p1;
+    thrust::device_vector<type> d_p2 = h_p2;
+    thrust::device_vector<type> d_result(n);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_p2.begin(), h_result.begin(), pow_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_p2.begin(), d_result.begin(), pow_functor());    
+    // pow can be very innacurate there's no point trying to check for equality
+    // Currently just checking for compilation
+    //    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), sqrt_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), sqrt_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+  }
+};
+VariableUnitTest<TestComplexPowerTransform, FloatingPointTypes> TestComplexPowerTransformInstance;
+
+template <typename T>
+struct TestComplexExponentialTransform
+{
+  void operator()(const size_t n)
+  {
+    typedef thrust::complex<T> type;
+    thrust::host_vector<type> h_p1 = random_complex_samples<T>(n);
+    thrust::host_vector<type>   h_result(n);
+
+    thrust::device_vector<type> d_p1 = h_p1;
+    thrust::device_vector<type> d_result(n);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), exp_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), exp_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), log_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), log_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), log10_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), log10_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+  }
+};
+VariableUnitTest<TestComplexExponentialTransform, FloatingPointTypes> TestComplexExponentialTransformInstance;
+
+template <typename T>
+struct TestComplexTrigonometricTransform
+{
+  void operator()(const size_t n)
+  {
+    typedef thrust::complex<T> type;
+    thrust::host_vector<type> h_p1 = random_complex_samples<T>(n);
+    thrust::host_vector<type>   h_result(n);
+
+    thrust::device_vector<type> d_p1 = h_p1;
+    thrust::device_vector<type> d_result(n);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), sin_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), sin_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), cos_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), cos_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), tan_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), tan_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), sinh_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), sinh_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), cosh_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), cosh_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), tanh_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), tanh_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), asin_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), asin_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), acos_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), acos_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), atan_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), atan_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), asinh_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), asinh_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), acosh_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), acosh_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+    thrust::transform(h_p1.begin(), h_p1.end(), h_result.begin(), atanh_functor());
+    thrust::transform(d_p1.begin(), d_p1.end(), d_result.begin(), atanh_functor());    
+    ASSERT_ALMOST_EQUAL(h_result, d_result);
+
+  }
+};
+VariableUnitTest<TestComplexTrigonometricTransform, FloatingPointTypes> TestComplexTrigonometricTransformInstance;
+
