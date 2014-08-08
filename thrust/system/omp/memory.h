d34d584ba35ae0c4bc69b8f54325617d77babc30
Jared Hoberock <jaredhoberock@gmail.com>
2014-08-08 16:40:11 -0700

    Eliminate specializations of thrust::iterator_traits which are no longer necessary.

--- thrust/system/omp/memory.h
+++ thrust/system/omp/memory.h
@@ -27,49 +27,6 @@
 #include <thrust/detail/allocator/malloc_allocator.h>
 #include <ostream>
 
-namespace thrust
-{
-namespace system
-{
-namespace omp
-{
-
-template<typename> class pointer;
-
-} // end omp
-} // end system
-} // end thrust
-
-
-/*! \cond
- */
-
-// specialize thrust::iterator_traits to avoid problems with the name of
-// pointer's constructor shadowing its nested pointer type
-// do this before pointer is defined so the specialization is correctly
-// used inside the definition
-namespace thrust
-{
-
-template<typename Element>
-  struct iterator_traits<thrust::system::omp::pointer<Element> >
-{
-  private:
-    typedef thrust::system::omp::pointer<Element> ptr;
-
-  public:
-    typedef typename ptr::iterator_category       iterator_category;
-    typedef typename ptr::value_type              value_type;
-    typedef typename ptr::difference_type         difference_type;
-    typedef ptr                                   pointer;
-    typedef typename ptr::reference               reference;
-}; // end iterator_traits
-
-} // end thrust
-
-/*! \endcond
- */
-
 
 namespace thrust
 {
