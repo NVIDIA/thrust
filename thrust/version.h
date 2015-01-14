27156b4a1ba8cfa48381efb21538ba1918cbc2f2
Jared Hoberock <jaredhoberock@gmail.com>
2015-01-14 11:45:30 -0800

    Bump version number to 1.8.1 Add stub CHANGELOG for 1.8.1

--- thrust/version.h
+++ thrust/version.h
@@ -47,7 +47,7 @@
  *         <tt>THRUST_VERSION / 100 % 1000</tt> is the minor version.
  *         <tt>THRUST_VERSION / 100000</tt> is the major version.
  */
-#define THRUST_VERSION 100800
+#define THRUST_VERSION 100801
 
 /*! \def THRUST_MAJOR_VERSION
  *  \brief The preprocessor macro \p THRUST_MAJOR_VERSION encodes the
