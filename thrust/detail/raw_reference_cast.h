c8d9eeff19b5a411d6a58f0e5541b36ba7472f9e
Andrew Corrigan <andrew.corrigan@gmail.com>
2015-01-25 10:51:26 -0500

    fixes include

--- thrust/detail/raw_reference_cast.h
+++ thrust/detail/raw_reference_cast.h
@@ -20,7 +20,7 @@
 #include <thrust/detail/raw_pointer_cast.h>
 #include <thrust/detail/type_traits/has_nested_type.h>
 #include <thrust/detail/type_traits.h>
-#include <thrust/detail/tuple_transform.h>
+#include <thrust/detail/tuple/tuple_transform.h>
 #include <thrust/iterator/detail/tuple_of_iterator_references.h>
 
 
