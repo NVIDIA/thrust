a97c1d4f69905f143f8f9ee307f8e4118bb27c72
Jared Hoberock <jaredhoberock@gmail.com>
2015-07-13 13:06:01 -0700

    it's -> its

--- thrust/functional.h
+++ thrust/functional.h
@@ -59,7 +59,7 @@ template<typename Operation> struct binary_traits;
  *  \endcode
  *
  *  \note Because C++11 language support makes the functionality of
- *        \c unary_function obsolete, it's use is optional if C++11 language
+ *        \c unary_function obsolete, its use is optional if C++11 language
  *        features are enabled.
  *
  *  \see http://www.sgi.com/tech/stl/unary_function.html
@@ -99,7 +99,7 @@ struct unary_function
  *  \endcode
  *
  *  \note Because C++11 language support makes the functionality of
- *        \c binary_function obsolete, it's use is optional if C++11 language
+ *        \c binary_function obsolete, its use is optional if C++11 language
  *        features are enabled.
  *
  *  \see http://www.sgi.com/tech/stl/binary_function.html
