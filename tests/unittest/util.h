4ff6cf42d3e66c212b9c42c9cf152d5c020d56fc
Evghenii Gaburov <egaburov@nvidia.com>
2015-11-19 16:09:15 -0800

    Revert "split uber-test binary into mulitple tests: one binary per source file"
    This reverts commit 82904eb168edb64155d9435121479d9d65f16f2b.

---
+++ tests/unittest/util.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <unittest/system.h>
+
+namespace unittest
+{
+
+template<typename T>
+  std::string type_name(void)
+{
+  return demangle(typeid(T).name());
+} // end type_name()
+
+} // end unittest
+
+template <typename Iterator>
+void PRINT(Iterator first, Iterator last)
+{
+  size_t n = 0;
+  for (Iterator i = first; i != last; i++, n++)
+    std::cout << ">>> [" << n << "] = " << *i << std::endl;
+}
+
+template <typename Container>
+void PRINT(const Container& c)
+{
+  PRINT(c.begin(), c.end());
+}
+
+template <size_t N>
+void PRINT(const char (&c)[N])
+{
+  std::cout << std::string(c, c + N) << std::endl;
+}
+
