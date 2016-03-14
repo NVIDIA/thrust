c258b2f1b64d48ef4b2ba67c444806fca6a0fad6
Evghenii Gaburov <egaburov@nvidia.com>
2016-03-14 15:12:54 -0700

    modify the example to gather multiple elements, which are used as a arguments to a function whose  single valued result is stored in an output array.

--- examples/transform_output_iterator.cu
+++ examples/transform_output_iterator.cu
@@ -4,28 +4,35 @@
 #include <thrust/iterator/transform_output_iterator.h>
 #include <iostream>
 
-struct Functor : thrust::unary_function<float,float>
+struct Functor 
 {
+  template<class Tuple>
   __host__ __device__
-  float operator()(float x)
+  float operator()(const Tuple& tuple) const
   {
-    return x*2.0f / 3.0f;
+    const float x = thrust::get<0>(tuple);
+    const float y = thrust::get<1>(tuple);
+    return x*y*2.0f / 3.0f;
   }
 };
 
 int main(void)
 {
-  float u[4] = {4, 3, 2, 1};
+  float u[4] = { 4 , 3,  2,   1};
+  float v[4] = {-1,  1,  1,  -1};
   int idx[3] = {3, 0, 1};
   float w[3] = {0, 0, 0};
 
   thrust::device_vector<float> U(u, u + 4);
+  thrust::device_vector<float> V(v, v + 4);
   thrust::device_vector<int> IDX(idx, idx + 3);
   thrust::device_vector<float> W(w, w + 3);
 
-  // gather elements and transform them before writing result in memory
-  thrust::gather(IDX.begin(), IDX.end(), U.begin(),
-                 thrust::make_transform_output_iterator(W.begin(), Functor()));
+  // gather multiple elements and apply a function before writing result in memory
+  thrust::gather(
+      IDX.begin(), IDX.end(),
+      thrust::make_zip_iterator(thrust::make_tuple(U.begin(), V.begin())),
+      thrust::make_transform_output_iterator(W.begin(), Functor()));
 
   std::cout << "result= [ ";
   for (size_t i = 0; i < 3; i++)
