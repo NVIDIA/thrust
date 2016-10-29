ce632c8f2610923031ffc4d7759fb6b6148ae6c9
Andrew Corrigan <andrew.corrigan@gmail.com>
2016-10-28 22:18:27 -0400

    resolves #725 and #850

--- thrust/iterator/detail/tuple_of_iterator_references.h
+++ thrust/iterator/detail/tuple_of_iterator_references.h
@@ -241,6 +241,20 @@ template<
 };
 
 
+// this overload of swap() permits swapping tuple_of_iterator_references returned as temporaries from
+// iterator dereferences
+template<
+  typename T0, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9,
+  typename U0, typename U1, typename U2, typename U3, typename U4, typename U5, typename U6, typename U7, typename U8, typename U9
+>
+inline __host__ __device__
+void swap(tuple_of_iterator_references<T0,T1,T2,T3,T4,T5,T6,T7,T8,T9> x,
+          tuple_of_iterator_references<U0,U1,U2,U3,U4,U5,U6,U7,U8,U9> y)
+{
+  x.swap(y);
+}
+
+
 } // end detail
 } // end thrust
 
