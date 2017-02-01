a88e6372ea353ec9fcd12120b026ed94aeabc6cc
Evghenii Gaburov <egaburov@nvidia.com>
2017-01-31 18:25:48 -0800

    Bump patch number
    [git-p4: depot-paths = "//sw/gpgpu/thrust/": change = 21651440]

--- thrust/version.h
+++ thrust/version.h
@@ -71,7 +71,7 @@
  *  \brief The preprocessor macro \p THRUST_PATCH_NUMBER encodes the
  *         patch number of the Thrust library.
  */
-#define THRUST_PATCH_NUMBER 3
+#define THRUST_PATCH_NUMBER 4
 
 
 // Declare these namespaces here for the purpose of Doxygenating them
