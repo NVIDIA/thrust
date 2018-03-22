6168145d4036b5771c12bb9bdc97beb9455a481b
Matthew Piechotka <mpiechotka@nvidia.com>
2018-03-22 11:52:59 -0800

    Remove use of CUDA RT deprecated functions from Thrust
    Bug 2035305
    Bug 2090310
    DVS_COMPUTE_SANITY all
    DVS_BASIC_SANITY cuda
    DVS_MODS_SANITY all
    Reviewed by blelbach
    
    Jobs: 2035305-2006 2090310-2006
    [git-p4: depot-paths = "//sw/gpgpu/thrust/": change = 23780359]

--- internal/benchmark/timer.h
+++ internal/benchmark/timer.h
@@ -12,7 +12,7 @@
 
 #  define CUDA_SAFE_CALL( call) do {                                         \
     CUDA_SAFE_CALL_NO_SYNC(call);                                            \
-    cudaError err = cudaThreadSynchronize();                                 \
+    cudaError err = cudaDeviceSynchronize();                                 \
     if( cudaSuccess != err) {                                                \
         fprintf(stderr, "Cuda error in file '%s' in line %i : %s.\n",        \
                 __FILE__, __LINE__, cudaGetErrorString( err) );              \
