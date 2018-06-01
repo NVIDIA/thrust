507cdd39719bd6517bd5d0e7b78097cb021d4165
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2018-05-31 23:36:26 -0800

    `iter_swap`: Fix integration mismatch. Bug 2004661
    Jobs: 2004661-2006
    [git-p4: depot-paths = "//sw/gpgpu/thrust/": change = 24263261]

--- thrust/system/cuda/detail/iter_swap.h
+++ thrust/system/cuda/detail/iter_swap.h
@@ -49,9 +49,9 @@ void iter_swap(thrust::cuda::execution_policy<DerivedPolicy> &, Pointer1 a, Poin
   };
 
 #ifndef __CUDA_ARCH__
-  return war_nvbugs_881631::host_path(a,b);
+  return war_nvbugs_881631::host_path(a, b);
 #else
-  return war_nvbugs_881631::device_path(a,b);
+  return war_nvbugs_881631::device_path(a, b);
 #endif // __CUDA_ARCH__
 } // end iter_swap()
 
