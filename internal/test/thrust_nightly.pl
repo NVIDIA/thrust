be5c58208e951d6a73532c0a34c66a940d2915d3
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2018-11-22 08:37:05 -0800

    Testing: Change the legacy testing driver to stop treating test binaries with no tests as failures.
    Bug 2379510

--- internal/test/thrust_nightly.pl
+++ internal/test/thrust_nightly.pl
@@ -469,8 +469,8 @@ sub run_unit_tests {
                     printf("#### WALLTIME $test %.2f [s]\n", $elapsed);
                 } elsif ($known_fail == 0 and $pass == 0) {
                     $errors = $errors + 1;
-                    printf("#### ERROR $test returned 0 and had no failures, known failures, errors or passes. Invalid test?\n");
-                    printf("&&&& FAILED $test\n");
+                    printf("#### DISABLED $test returned 0 and had no failures, known failures, errors or passes.\n");
+                    printf("&&&& PASSED $test\n");
                     printf("#### WALLTIME $test %.2f [s]\n", $elapsed);
                 } else {
                     printf("&&&& PASSED $test\n");
