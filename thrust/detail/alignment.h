a6d41f66efde6bf8d7220aa6013b5ffa8cd95122
Michał 'Griwes' Dominiak <griwes@griwes.info>
2019-04-15 20:12:03 +0200

    Properly host-device qualify some previously unqualified functions.
    Bug 2422333
    Bug 2522259
    Bug 2528822
    Github #949
    Github #973

--- thrust/detail/alignment.h
+++ thrust/detail/alignment.h
@@ -208,11 +208,13 @@ struct aligned_type;
 /// \p aligned_reinterpret_cast is responsible for ensuring that the alignment
 /// requirements are actually satisified.
 template <typename T, typename U>
+__host__ __device__
 T aligned_reinterpret_cast(U u)
 {
   return reinterpret_cast<T>(reinterpret_cast<void*>(u));
 }
 
+__host__ __device__
 inline std::size_t aligned_storage_size(std::size_t n, std::size_t align)
 {
   return ((n + align - 1) / align) * align;
