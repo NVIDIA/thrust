05288a698f0be37a63c9682db9f6dacfdb1efd6b
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2019-10-28 16:01:24 -0700

    Fix new tuple algorithm tests to compile in C++11 mode.

--- testing/tuple_algorithms.cu
+++ testing/tuple_algorithms.cu
@@ -5,14 +5,26 @@
 #include <unittest/unittest.h>
 
 #include <thrust/detail/tuple_algorithms.h>
+#include <thrust/type_traits/integer_sequence.h>
 
 // FIXME: Replace with C++14 style `thrust::square<>` when we have it.
 struct custom_square
 {
   template <typename T>
+  __host__ __device__
   T operator()(T v) const
   {
-    return v * v; 
+    return v * v;
+  }
+};
+
+struct custom_square_inplace
+{
+  template <typename T>
+  __host__ __device__
+  void operator()(T& v) const
+  {
+    v *= v;
   }
 };
 
@@ -20,7 +32,7 @@ void test_tuple_subset()
 {
   auto t0 = std::make_tuple(0, 2, 3.14);
 
-  auto t1 = thrust::tuple_subset(t0, std::index_sequence<2, 0>{}); 
+  auto t1 = thrust::tuple_subset(t0, thrust::index_sequence<2, 0>{});
 
   ASSERT_EQUAL_QUIET(t1, std::make_tuple(3.14, 0));
 }
@@ -30,7 +42,7 @@ void test_tuple_transform()
 {
   auto t0 = std::make_tuple(0, 2, 3.14);
 
-  auto t1 = thrust::tuple_transform(t0, custom_square{}); 
+  auto t1 = thrust::tuple_transform(t0, custom_square{});
 
   ASSERT_EQUAL_QUIET(t1, std::make_tuple(0, 4, 9.8596));
 }
@@ -40,11 +52,11 @@ void test_tuple_for_each()
 {
   auto t = std::make_tuple(0, 2, 3.14);
 
-  thrust::tuple_for_each(t, [](auto& x) { x *= x; }); 
+  thrust::tuple_for_each(t, custom_square_inplace{});
 
   ASSERT_EQUAL_QUIET(t, std::make_tuple(0, 4, 9.8596));
 }
 DECLARE_UNITTEST(test_tuple_for_each);
- 
+
 #endif // THRUST_CPP_DIALECT >= 2011
 
