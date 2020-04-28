27216faee5e61c3dc8513515a817e0c23fef1967
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2020-04-28 10:23:09 -0700

    Bump version to 1.9.10. Commits prior to this are in Thrust 1.9.9.

--- thrust/version.h
+++ thrust/version.h
@@ -47,7 +47,7 @@
  *         <tt>THRUST_VERSION / 100 % 1000</tt> is the minor version.
  *         <tt>THRUST_VERSION / 100000</tt> is the major version.
  */
-#define THRUST_VERSION 100909
+#define THRUST_VERSION 100910
 
 /*! \def THRUST_MAJOR_VERSION
  *  \brief The preprocessor macro \p THRUST_MAJOR_VERSION encodes the
