bca02bf39b75163c1723b96a28394cc22be2667b
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2020-10-13 21:53:49 -0700

    Docker: - Run `ldconfig` as root in local containerized builds to workaround issues with   nvidia-docker2 and Debian. - Add a "clean" option to local containzered builds. - Stop using the "c++" and "cu++" symlinks, because NVCC doesn't like being   symlinked to (it breaks nvcc.profile). - Add script for determining parallelism, based on the one we used with DVS.

--- ci/common/build.bash
+++ ci/common/build.bash
@@ -20,6 +20,9 @@ function logger() {
 # VARIABLES - Set up bash and environmental variables.
 ################################################################################
 
+# Get the variables the Docker container set up for us: ${CXX}, ${CUDACXX}, etc.
+source /etc/cccl.bashrc
+
 # Set path and build parallel level
 export PATH=/usr/local/cuda/bin:${PATH}
 
@@ -30,34 +33,34 @@ export HOME=${WORKSPACE}
 cd ${WORKSPACE}
 
 # If it's a nightly build, append current YYMMDD to version.
-if [[ "${BUILD_MODE}" = "branch" ]]; then
+if [ "${BUILD_MODE}" == "branch" ]; then
   export VERSION_SUFFIX=`date +%y%m%d`
 fi
 
-# The Docker image sets up `c++` and `cu++`.
-CMAKE_FLAGS="-DCMAKE_CXX_COMPILER=c++ -DCMAKE_CUDA_COMPILER=cu++"
+# The Docker image sets up `${CXX}` and `${CUDACXX}`.
+CMAKE_FLAGS="-DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_CUDA_COMPILER=${CUDACXX}"
 
 # If it's a nightly build, build all configurations.
-if [[ "${BUILD_MODE}" = "branch" ]]; then
+if [ "${BUILD_MODE}" == "branch" ]; then
   CMAKE_FLAGS="${CMAKE_FLAGS} -DTHRUST_MULTICONFIG_WORKLOAD=FULL"
 fi
 
 CTEST_FLAGS=""
 
-if [[ "${BUILD_KIND}" = "cpu" ]]; then
+if [ "${BUILD_KIND}" == "cpu" ]; then
   CTEST_FLAGS="${CTEST_FLAGS} -E '^cub|^thrust.*cuda'"
 fi
 
 ################################################################################
-# ENVIRONMENT - Print out information about the environment.
+# ENVIRONMENT - Configure and print out information about the environment.
 ################################################################################
 
 logger "Get environment..."
 env
 
 logger "Check versions..."
-c++ --version
-cu++ --version
+${CXX} --version
+${CUDACXX} --version
 
 ################################################################################
 # BUILD - Build Thrust and CUB examples and tests.
@@ -66,11 +69,16 @@ cu++ --version
 mkdir -p build
 cd build
 
-logger "Configure Thrust and CUB..."
-cmake ${CMAKE_FLAGS} ..
+if [ ! -f CMakeLists.txt ]; then
+  logger "Configure Thrust and CUB..."
+  cmake ${CMAKE_FLAGS} ..
+else
+  logger "Existing Thrust and CUB configuration found, skipping configure..."
+fi
 
 logger "Build Thrust and CUB..."
-cmake --build . -j "${1}"
+source ../ci/common/determine_build_parallelism.bash
+cmake --build . -j${BUILD_THREADS} "${@}"
 
 ################################################################################
 # TEST - Run Thrust and CUB examples and tests.
