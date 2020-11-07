1f86bd12b0a870d72b97873cde7c6ac6e6310dd7
Anatoliy Tomilov <tomilovanatoliy@gmail.com>
2020-11-07 14:55:57 +0500

    Fix type trait for GCC 10.2.

--- thrust/detail/type_traits.h
+++ thrust/detail/type_traits.h
@@ -131,7 +131,7 @@ template<typename T> struct is_pod
        || __is_pod(T)
 #elif THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_GCC
 // only use the intrinsic for >= 4.3
-#if (__GNUC__ >= 4) && (__GNUC_MINOR__ >= 3)
+#if (__GNUC__ * 100 + __GNUC_MINOR__ >= 403)
        || __is_pod(T)
 #endif // GCC VERSION
 #endif // THRUST_HOST_COMPILER
