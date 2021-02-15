1564cb585159eda01d9f8b397297529636a84298
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2021-02-15 12:17:36 -0800

    Docs: remove "outputDir", "debugTemplateJson", and "useFolders" from the Doxybook JSON config, as they can only be set on the command line.

--- thrust/device_ptr.h
+++ thrust/device_ptr.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 NVIDIA Corporation
+ *  Copyright 2008-2021 NVIDIA Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -164,8 +164,8 @@ class device_ptr
      */
     __host__ __device__
     T* get() const;
-#endif // end doxygen-only members
-}; // end device_ptr
+#endif
+};
 
 #if THRUST_DOXYGEN
 /*! Write the address that a \c device_ptr points to to an output stream.
