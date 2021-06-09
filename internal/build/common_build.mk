506c82b0ab72f26e1d2cfb823226357bfff985cc
Allison Vacanti <alliepiper16@gmail.com>
2021-06-09 17:41:44 -0400

    Update deprecation mechanism (opt-out, cmake, c++14).
    Opt-out for our internal builds, since we'll still need to maintain and
    test any deprecated APIs.

--- internal/build/common_build.mk
+++ internal/build/common_build.mk
@@ -6,6 +6,10 @@ ifeq ($(OS),Linux)
   LIBRARIES += m
 endif
 
+# Disable our THRUST_DEPRECATED and CUB_DEPRECATED macros for internal
+# builds, since we need to build and test our deprecated APIs with -Werror.
+CUDACC_FLAGS += -DTHRUST_IGNORE_DEPRECATED_API
+
 include $(ROOTDIR)/thrust/internal/build/common_compiler.mk
 
 # Add /bigobj to Windows build flag to workaround building Thrust with debug
