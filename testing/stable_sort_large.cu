6dae59baef858f82976cdf1c83806a913080f639
Allison Vacanti <alliepiper16@gmail.com>
2022-07-15 16:57:23 -0400

    WAR NVbug 3714031.

--- testing/stable_sort_large.cu
+++ testing/stable_sort_large.cu
@@ -1,45 +1,34 @@
-#include <unittest/unittest.h>
-#include <thrust/sort.h>
 #include <thrust/functional.h>
-
+#include <thrust/sort.h>
+#include <unittest/unittest.h>
 
 template <typename T, unsigned int N>
-void _TestStableSortWithLargeKeys(void)
+void _TestStableSortWithLargeKeys()
 {
-    size_t n = (128 * 1024) / sizeof(FixedVector<T,N>);
+  size_t n = (128 * 1024) / sizeof(FixedVector<T, N>);
 
-    thrust::host_vector< FixedVector<T,N> > h_keys(n);
+  thrust::host_vector<FixedVector<T, N>> h_keys(n);
 
-    for(size_t i = 0; i < n; i++)
-        // XXX Use proper random number generation facility.
-        h_keys[i] = FixedVector<T,N>(rand());
+  for (size_t i = 0; i < n; i++)
+  {
+    // XXX Use proper random number generation facility.
+    h_keys[i] = FixedVector<T, N>(rand());
+  }
 
-    thrust::device_vector< FixedVector<T,N> > d_keys = h_keys;
-    
-    thrust::stable_sort(h_keys.begin(), h_keys.end());
-    thrust::stable_sort(d_keys.begin(), d_keys.end());
+  thrust::device_vector<FixedVector<T, N>> d_keys = h_keys;
 
-    ASSERT_EQUAL_QUIET(h_keys, d_keys);
+  thrust::stable_sort(h_keys.begin(), h_keys.end());
+  thrust::stable_sort(d_keys.begin(), d_keys.end());
+
+  ASSERT_EQUAL_QUIET(h_keys, d_keys);
 }
 
-void TestStableSortWithLargeKeys(void)
+void TestStableSortWithLargeKeys()
 {
-    _TestStableSortWithLargeKeys<int,    1>();
-    _TestStableSortWithLargeKeys<int,    2>();
-    _TestStableSortWithLargeKeys<int,    4>();
-    _TestStableSortWithLargeKeys<int,    8>();
-    _TestStableSortWithLargeKeys<int,   16>();
-    _TestStableSortWithLargeKeys<int,   32>();
-    _TestStableSortWithLargeKeys<int,   64>();
-    _TestStableSortWithLargeKeys<int,  128>();
-    _TestStableSortWithLargeKeys<int,  256>();
-
-// XXX these take too long to compile
-//    _TestStableSortWithLargeKeys<int,  512>();
-//    _TestStableSortWithLargeKeys<int, 1024>();
-//    _TestStableSortWithLargeKeys<int, 2048>();
-//    _TestStableSortWithLargeKeys<int, 4096>();
-//    _TestStableSortWithLargeKeys<int, 8192>();
+#ifdef _NVHPC_CUDA // WAR NVBug 3720703
+  _TestStableSortWithLargeKeys<int, 128>();
+#else
+  _TestStableSortWithLargeKeys<int, 256>();
+#endif
 }
 DECLARE_UNITTEST(TestStableSortWithLargeKeys);
-
