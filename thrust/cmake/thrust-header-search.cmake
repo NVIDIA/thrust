48b03f1421e798024389cd53f2cd8b0dcb2dbed7
Robert Maynard <robertjmaynard@gmail.com>
2022-07-20 21:38:51 -0400

    Update thrust/cmake/thrust-header-search.cmake
    Co-authored-by: Allison Vacanti <alliepiper16@gmail.com>

--- thrust/cmake/thrust-header-search.cmake
+++ thrust/cmake/thrust-header-search.cmake
@@ -1,5 +1,4 @@
 # Parse version information from version.h in source tree
- # Source tree
 set(_THRUST_VERSION_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../..")
 if(EXISTS "${_THRUST_VERSION_INCLUDE_DIR}/thrust/version.h")
   set(_THRUST_VERSION_INCLUDE_DIR "${_THRUST_VERSION_INCLUDE_DIR}" CACHE FILEPATH "" FORCE) # Clear old result
